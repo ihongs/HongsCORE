c06d6744acd792a0dd0144b8f6acebe7e283b6ae
Hongs <Hongs@hongs-mba.lan>
2017-02-27 09:20:57 +0800

    完善会话期限相关逻辑

--- hongs-web/web/WEB-INF/bin/_setup_/00.common.sql
+++ hongs-web/web/WEB-INF/bin/_setup_/00.common.sql
@@ -7,8 +7,11 @@
 DROP TABLE IF EXISTS `a_common_record`;
 CREATE TABLE `a_common_record` (
     `id` VARCHAR(64) NOT NULL,
-    `data` BLOB NOT NULL,
-    `ctime` UNSIGNED INT(11) DEFAULT 0,
+    `data`  BLOB NOT NULL,
     `xtime` UNSIGNED INT(11) DEFAULT 0,
+    `mtime` UNSIGNED INT(11) DEFAULT 0,
     PRIMARY KEY (`id`)
 );
+
+CREATE INDEX `IK_a_common_record_xtime` ON `a_member_dept` (`xtime`);
+CREATE INDEX `IK_a_common_record_mtime` ON `a_member_dept` (`mtime`);

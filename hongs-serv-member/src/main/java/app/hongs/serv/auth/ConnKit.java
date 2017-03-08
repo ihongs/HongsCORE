84c4bd774849ba30199641e34e0ebaedb3d22cf3
Hongs <Hongs@hongs-mba.lan>
2017-03-09 00:32:46 +0800

    改为调用 app.hongs.util.Http 相关方法

--- hongs-serv-member/src/main/java/app/hongs/serv/auth/ConnKit.java
+++ hongs-serv-member/src/main/java/app/hongs/serv/auth/ConnKit.java
@@ -3,36 +3,25 @@ package app.hongs.serv.auth;
 import app.hongs.Core;
 import app.hongs.CoreConfig;
 import app.hongs.HongsException;
-import app.hongs.HongsUnchecked;
 import app.hongs.action.ActionHelper;
-import app.hongs.util.Data;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import javax.net.ssl.SSLContext;
-import java.security.cert.X509Certificate;
-import java.security.cert.CertificateException;
-import java.util.ArrayList;
-import java.util.List;
+import app.hongs.util.Http;
 import java.util.Map;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContextBuilder;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
 
 public class ConnKit {
+
+    /**
+     * 执行远程请求
+     * 协议支持 HTTP,HTTPS
+     * 数据支持 JSON,JSONP,QueryString
+     * @param url
+     * @param req
+     * @return
+     * @throws HongsException
+     */
+    public static Map retrieve(String url, Map<String, Object> req)
+            throws HongsException {
+        return Http.parseData(Http.request(url, req));
+    }
 
     /**
      * 登录成功后跳转
@@ -78,84 +67,6 @@ public class ConnKit {
 
         // 跳转
         helper.redirect(v);
-    }
-
-    /**
-     * 执行远程请求
-     * 协议支持 HTTP,HTTPS
-     * 数据支持 JSON,QueryString
-     * @param url
-     * @param req
-     * @return
-     * @throws HongsException
-     */
-    public static Map requests(String url, Map<String, String> req)
-            throws HongsException {
-        try {
-            HttpPost post = new HttpPost();
-            post.setURI   ( new URI(url) );
-
-            List<NameValuePair> data = new ArrayList( );
-            for (Map.Entry<String, String> et : req.entrySet()) {
-                String n = et.getKey(  );
-                String v = et.getValue();
-                data.add(new BasicNameValuePair(n , v));
-            }
-            post.setEntity(new UrlEncodedFormEntity(data, HTTP.UTF_8));
-
-            HttpResponse resp = (
-                    ! url.startsWith("https://" )
-                    ? HttpClients.createDefault()
-                    : createHttps()
-                    ).execute(post);
-            if (resp.getStatusLine().getStatusCode() != 200) {
-                throw new HongsException.Common("Call "+url+" Response code: "+resp.getStatusLine().getStatusCode());
-            }
-
-            String text = EntityUtils.toString(resp.getEntity()).trim();
-            if (text.startsWith("{") && text.endsWith("}")) {
-                return (Map) Data.toObject(text);
-            } else
-            if (text.startsWith("callback(")) { // QQ 获取 access_token 返回的数据格式
-                return (Map) Data.toObject(text.substring(9,text.length()-2));
-            } else {
-                return ActionHelper.parseParam(ActionHelper.parseQuery(text));
-            }
-        } catch (UnsupportedEncodingException ex) {
-            throw new HongsUnchecked.Common(ex);
-        } catch (URISyntaxException ex) {
-            throw new HongsUnchecked.Common(ex);
-        } catch (IOException ex) {
-            throw new HongsUnchecked.Common(ex);
-        }
-    }
-
-    private static CloseableHttpClient createHttps() {
-        try {
-            SSLContext                 sslContext;
-            SSLConnectionSocketFactory sslConnect;
-
-            sslContext = new SSLContextBuilder()
-                    .loadTrustMaterial(null, new TrustStrategy() {
-                @Override
-                public boolean isTrusted(X509Certificate[] chain, String authType)
-                throws CertificateException {
-                    return true;
-                }
-            }).build();
-
-            sslConnect = new SSLConnectionSocketFactory(sslContext);
-            return HttpClients
-                    .custom()
-                    .setSSLSocketFactory(sslConnect)
-                    .build ();
-        } catch (NoSuchAlgorithmException ex) {
-            throw new HongsUnchecked.Common(ex);
-        } catch (KeyManagementException ex) {
-            throw new HongsUnchecked.Common(ex);
-        } catch (KeyStoreException ex) {
-            throw new HongsUnchecked.Common(ex);
-        }
     }
 
 }

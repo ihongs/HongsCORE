16927cb569a23b012f8252636de2a746cdcf0f85
Hongs <Hongs@hongs-mba.lan>
2017-03-15 00:57:57 +0800

    增加对任意可序列化对象转为 JSON 的方法

--- hongs-core/src/main/java/app/hongs/jsp/Pagelet.java
+++ hongs-core/src/main/java/app/hongs/jsp/Pagelet.java
@@ -98,7 +98,7 @@ public class Pagelet extends ActionDriver implements HttpJspPage
    * @return
    */
   public static String escapeJSS(String str) {
-      return Data.escape  (str);
+      return Data.toEscape(str);
   }
 
   /**

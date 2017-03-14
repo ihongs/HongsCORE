35bd1018b0d44d014c07492de9406bfddd3ddead
Hongs <ihongs@live.cn>
2017-03-14 23:41:12 +0800

    增加独立的会话数据表

--- hongs-web/web/WEB-INF/bin/_setup_/00.common.sql
+++ hongs-web/web/WEB-INF/bin/_setup_/00.common.sql
@@ -6,7 +6,7 @@
 
 DROP TABLE IF EXISTS `a_common_record`;
 CREATE TABLE `a_common_record` (
-    `id` VARCHAR(64) NOT NULL,
+    `id` VARCHAR(96) NOT NULL,
     `data`  BLOB NOT NULL,
     `xtime` UNSIGNED INT(11) DEFAULT 0,
     `mtime` UNSIGNED INT(11) DEFAULT 0,
@@ -15,3 +15,19 @@ CREATE TABLE `a_common_record` (
 
 CREATE INDEX `IK_a_common_record_xtime` ON `a_common_record` (`xtime`);
 CREATE INDEX `IK_a_common_record_mtime` ON `a_common_record` (`mtime`);
+
+--
+-- 会话数据存储
+--
+
+DROP TABLE IF EXISTS `a_common_sesion`;
+CREATE TABLE `a_common_sesion` (
+    `id` VARCHAR(96) NOT NULL,
+    `data`  BLOB NOT NULL,
+    `xtime` UNSIGNED INT(11) DEFAULT 0,
+    `mtime` UNSIGNED INT(11) DEFAULT 0,
+    PRIMARY KEY (`id`)
+);
+
+CREATE INDEX `IK_a_common_sesion_xtime` ON `a_common_sesion` (`xtime`);
+CREATE INDEX `IK_a_common_sesion_mtime` ON `a_common_sesion` (`mtime`);

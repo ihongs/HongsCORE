95ec6513f258a81075403c9017950d126faadbda
Hongs <Hongs@hongs-mba.lan>
2017-05-25 02:23:40 +0800

    增加全局资源锁

--- hongs-serv-search/src/main/java/app/hongs/dh/search/SearchRecord.java
+++ hongs-serv-search/src/main/java/app/hongs/dh/search/SearchRecord.java
@@ -3,34 +3,35 @@ package app.hongs.dh.search;
 import app.hongs.Cnst;
 import app.hongs.Core;
 import app.hongs.HongsException;
+import app.hongs.HongsExpedient;
 import app.hongs.action.FormSet;
 import app.hongs.dh.lucene.LuceneRecord;
-import app.hongs.util.Synt;
+import app.hongs.util.Lock;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.Term;
 
 /**
  * 搜索记录
- * 避免同时写入而产生异常
- * <p>
- * 此类中的 add,put,set和del 不会立即写库,
- * 而是调用 SearchQueuer.add 排队等待写入.
- * </p>
+ *
+ * 增加写锁避免同时写入导致失败
+ * 注意: 此类的对象无法使用事务
+ *
  * @author Hongs
  */
 public class SearchRecord extends LuceneRecord {
-
-    public SearchRecord(String path, Map form) throws HongsException {
-        super(path, form);
-    }
 
     public SearchRecord(String path) throws HongsException {
         super(path);
     }
 
-    public SearchQueuer getQueuer( ) throws HongsException {
-        return  SearchQueuer.getInstance(this);
+    public SearchRecord(String path, Map form) throws HongsException {
+        super(path, form);
     }
 
     /**
@@ -59,115 +60,64 @@ public class SearchRecord extends LuceneRecord {
         return inst;
     }
 
-    /**
-     * 添加文档
-     * @param rd
-     * @return ID
-     * @throws HongsException
-     */
     @Override
-    public String add(Map rd) throws HongsException {
-        String id = Synt.declare(rd.get(Cnst.ID_KEY), String.class);
-        if (id != null && id.length() != 0) {
-            throw new HongsException.Common("Id can not set in add");
-        }
-        id = Core.newIdentity();
-        rd.put(Cnst.ID_KEY, id);
-
-        // 放入索引队列
-        //addDoc(map2Doc(rd));
-        rd = new HashMap(rd);
-        rd.put("__action__", "set");
-        getQueuer( ).add(rd);
-
-        return id;
+    public void addDoc(final Document doc) throws HongsException {
+        final SearchRecord that = this;
+        final String key = SearchRecord.class.getName() + ":" + getDbName();
+        Lock.locker( key , new Runnable( ) {
+            @Override
+            public void run() {
+                try {
+                    IndexWriter iw = that.getWriter( );
+                    iw.addDocument (doc);
+                    iw.commit();
+                } catch (   IOException e) {
+                    throw new HongsExpedient.Common(e);
+                } catch (HongsException e) {
+                    throw e.toExpedient( );
+                }
+            }
+        });
     }
 
-    /**
-     * 设置文档(无则添加)
-     * @param id
-     * @param rd
-     * @throws HongsException
-     */
     @Override
-    public void set(String id, Map rd) throws HongsException {
-        if (id == null || id.length() == 0) {
-            throw new HongsException.Common("Id must be set in put");
-        }
-        Document doc = getDoc(id);
-        if (doc == null) {
-//          doc =  new Document();
-        } else {
-            /**
-             * 实际运行中发现
-             * 直接往取出的 doc 里设置属性, 会造成旧值的索引丢失
-             * 故只好转换成 map 再重新设置, 这样才能确保索引完整
-             * 但那些 Store=NO 的数据将无法设置
-             */
-            setView(new HashMap());
-            Map  md = doc2Map(doc);
-            md.putAll(rd);
-            rd = md;
-        }
-        rd.put(Cnst.ID_KEY, id);
-
-        // 放入索引队列
-        //docAdd(doc, rd);
-        //setDoc(id, doc);
-        rd = new HashMap(rd);
-        rd.put("__action__", "set");
-        getQueuer( ).add(rd);
+    public void setDoc(final String id, final Document doc) throws HongsException {
+        final SearchRecord that = this;
+        final String key = SearchRecord.class.getName() + ":" + getDbName();
+        Lock.locker( key , new Runnable( ) {
+            @Override
+            public void run() {
+                try {
+                    IndexWriter iw = that.getWriter( );
+                    iw.updateDocument (new Term(Cnst.ID_KEY, id), doc);
+                    iw.commit();
+                } catch (   IOException e) {
+                    throw new HongsExpedient.Common(e);
+                } catch (HongsException e) {
+                    throw e.toExpedient( );
+                }
+            }
+        });
     }
 
-    /**
-     * 修改文档(局部更新)
-     * @param id
-     * @param rd
-     * @throws HongsException
-     */
     @Override
-    public void put(String id, Map rd) throws HongsException {
-        if (id == null || id.length() == 0) {
-            throw new HongsException.Common("Id must be set in put");
-        }
-        Document doc = getDoc(id);
-        if (doc == null) {
-            throw new HongsException.Common("Doc#"+id+" not exists");
-        } else {
-            /**
-             * 实际运行中发现
-             * 直接往取出的 doc 里设置属性, 会造成旧值的索引丢失
-             * 故只好转换成 map 再重新设置, 这样才能确保索引完整
-             * 但那些 Store=NO 的数据将无法设置
-             */
-            setView(new HashMap());
-            Map  md = doc2Map(doc);
-            md.putAll(rd);
-            rd = md;
-        }
-        rd.put(Cnst.ID_KEY, id);
-
-        // 放入索引队列
-        //docAdd(doc, rd);
-        //setDoc(id, doc);
-        rd = new HashMap(rd);
-        rd.put("__action__", "set");
-        getQueuer( ).add(rd);
-    }
-
-    /**
-     * 删除文档(delDoc 的别名)
-     * @param id
-     * @throws HongsException
-     */
-    @Override
-    public void del(String id) throws HongsException {
-        //放入索引队列
-        //delDoc(id);
-        Map rd = new HashMap();
-        rd.put("__action__", "set");
-        rd.put(Cnst.ID_KEY ,  id  );
-        getQueuer( ).add( rd );
+    public void delDoc(final String id) throws HongsException {
+        final SearchRecord that = this;
+        final String key = SearchRecord.class.getName() + ":" + getDbName();
+        Lock.locker( key , new Runnable( ) {
+            @Override
+            public void run() {
+                try {
+                    IndexWriter iw = that.getWriter( );
+                    iw.deleteDocuments(new Term(Cnst.ID_KEY, id) /**/);
+                    iw.commit();
+                } catch (   IOException e) {
+                    throw new HongsExpedient.Common(e);
+                } catch (HongsException e) {
+                    throw e.toExpedient( );
+                }
+            }
+        });
     }
 
 }

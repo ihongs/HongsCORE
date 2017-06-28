ba103d6f6d90f5918ba7dbbe475d65fa0460d2a0
Hongs <Hongs@hongs-mba.lan>
2017-06-29 06:27:50 +0800

    原来的统计参数 top,cnt 改为与其他接口通用的 rn,rb

--- hongs-serv-search/src/main/java/app/hongs/dh/lucene/value/NumeraValue.java
+++ hongs-serv-search/src/main/java/app/hongs/dh/lucene/value/NumeraValue.java
@@ -7,7 +7,7 @@ import org.apache.lucene.index.IndexableField;
  *
  * @author Hongs
  */
-public class NumStrValue implements IValue {
+public class NumeraValue implements IValue {
     @Override
     public Object get(IndexableField f) {
         return Tool.toNumStr(f.numericValue());

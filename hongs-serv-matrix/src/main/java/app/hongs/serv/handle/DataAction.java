5e3a30b775eb178ae0091406c9659b5ad0ba2d8f
Hongs <ihongs@live.cn>
2017-06-29 22:42:29 +0800

    改为从 SearchAction 继承

--- hongs-serv-matrix/src/main/java/app/hongs/serv/handle/DataAction.java
+++ hongs-serv-matrix/src/main/java/app/hongs/serv/handle/DataAction.java
@@ -8,31 +8,33 @@ import app.hongs.action.anno.Action;
 import app.hongs.action.anno.Select;
 import app.hongs.action.anno.Spread;
 import app.hongs.action.anno.Verify;
-import app.hongs.dh.lucene.LuceneAction;
 import app.hongs.dh.lucene.LuceneRecord;
+import app.hongs.dh.search.SearchAction;
 import app.hongs.serv.matrix.Data;
 import java.lang.reflect.Method;
+import java.util.Map;
 
 /**
  * 数据存储动作
  * @author Hongs
  */
 @Action("handle/auto")
-public class DataAction extends LuceneAction {
+public class DataAction extends SearchAction {
 
     @Override
     public void acting(ActionHelper helper, ActionRunner runner)
     throws HongsException {
-        super.acting(helper, runner);
+        super.acting(helper , runner);
 
         // 绑定特制的表单
-        Method m = runner.getMethod( );
+        Method m = runner.getMethod();
         if (m.isAnnotationPresent(Select.class)
         ||  m.isAnnotationPresent(Spread.class)
         ||  m.isAnnotationPresent(Verify.class)) {
-            helper.setAttribute("form:"+mod+"/"+ent+"."+ent, getEntity(helper).getFields());
+            Map fcs = getEntity(helper).getFields();
+            helper.setAttribute("form:"+mod+"/"+ent+"."+ent, fcs);
         }
-        
+
         // 放入当前用户ID
         helper.getRequestData().put("cuid", helper.getSessibute(Cnst.UID_SES));
     }
--- hongs-serv-matrix/src/main/java/app/hongs/serv/manage/DataAction.java
+++ hongs-serv-matrix/src/main/java/app/hongs/serv/manage/DataAction.java
@@ -12,54 +12,44 @@ import app.hongs.action.anno.Preset;
 import app.hongs.action.anno.Select;
 import app.hongs.action.anno.Spread;
 import app.hongs.action.anno.Verify;
-import app.hongs.dh.IEntity;
-import app.hongs.dh.lucene.LuceneAction;
 import app.hongs.dh.lucene.LuceneRecord;
 import app.hongs.dh.lucene.LuceneRecord.Loop;
-import app.hongs.dh.search.SearchHelper;
+import app.hongs.dh.search.SearchAction;
 import app.hongs.serv.matrix.Data;
 import app.hongs.util.Synt;
 import app.hongs.util.Tool;
 import java.io.IOException;
 import java.io.Writer;
 import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * 数据存储动作
  * @author Hongs
  */
 @Action("manage/auto")
-public class DataAction extends LuceneAction {
+public class DataAction extends SearchAction {
 
-    protected Set<String> sub = Synt.asSet("counts", "statis", "export");
+    public DataAction() {
+        sub.add("export");
+    }
 
     @Override
     public void acting(ActionHelper helper, ActionRunner runner)
     throws HongsException {
-        // 特别扩展的资源
-        ent = runner.getEntity();
-        mod = runner.getModule();
-        if (sub.contains(ent)) {
-            int pos = mod.lastIndexOf("/");
-            ent = mod.substring(1+pos);
-            mod = mod.substring(0,pos);
-            runner.setEntity(ent);
-            runner.setModule(mod);
-        }
-
-        super.acting(helper, runner);
+        super.acting(helper , runner);
 
         // 绑定特制的表单
-        Method m = runner.getMethod( );
+        Method m = runner.getMethod();
         if (m.isAnnotationPresent(Select.class)
         ||  m.isAnnotationPresent(Spread.class)
         ||  m.isAnnotationPresent(Verify.class)) {
-            helper.setAttribute("form:"+mod+"/"+ent+"."+ent, getEntity(helper).getFields());
+            Map fcs = getEntity(helper).getFields();
+            helper.setAttribute("form:"+mod+"/"+ent+"."+ent, fcs);
         }
 
         // 放入当前用户ID
@@ -105,52 +95,15 @@ public class DataAction extends LuceneAction {
         helper.reply(ss, Synt.asMap("id",id));
     }
 
-    @Action("counts/search")
-    @Preset(conf="", envm="")
-    public void counts(ActionHelper helper) throws HongsException {
-        LuceneRecord sr = getEntity(helper);
-        SearchHelper sh = new SearchHelper(sr);
-        Map rd = helper.getRequestData();
-            rd = getReqMap (helper, sr, "counts", rd);
-        Map sd = sh.counts (rd);
-            sd = getRspMap (helper, sr, "counts", sd);
-
-        /**
-         * 追加枚举名称
-         */
-        Map xd = (Map) sd.get("info");
-       byte md = Synt.declare(helper.getParameter("md") , (byte) 0);
-        if (md != 0 && xd != null && mod != null && ent != null) {
-            if (FormSet.hasConfFile( mod )) {
-                sh.addLabel(xd , md, mod, ent);
-            }
-        }
-
-        helper.reply(sd);
+    @Action("redo")
+    @CommitSuccess
+    public void redo(ActionHelper helper) {
+        // TODO: Redo history updates
     }
 
-    @Action("statis/search")
-    @Preset(conf="", envm="")
-    public void statis(ActionHelper helper) throws HongsException {
-        LuceneRecord sr = getEntity(helper);
-        SearchHelper sh = new SearchHelper(sr);
-        Map rd = helper.getRequestData();
-            rd = getReqMap (helper, sr, "statis", rd);
-        Map sd = sh.statis (rd);
-            sd = getRspMap (helper, sr, "statis", sd);
-
-        /**
-         * 追加枚举名称
-         */
-        Map xd = (Map) sd.get("info");
-       byte md = Synt.declare(helper.getParameter("md") , (byte) 0);
-        if (md != 0 && xd != null && mod != null && ent != null) {
-            if (FormSet.hasConfFile( mod )) {
-                sh.addLabel(xd , md, mod, ent);
-            }
-        }
-
-        helper.reply(sd);
+    @Action("logs")
+    public void list(ActionHelper helper) {
+        // TODO: Get the history logs
     }
 
     @Action("export/search")
@@ -165,56 +118,64 @@ public class DataAction extends LuceneAction {
          * 逐行输出
          */
 
-        Writer  ot = helper.getOutputWriter();
         Map     fs = sr.getFields();
         Map     es = new HashMap( );
+        Writer  ot = helper.getOutputWriter();
+
+        String csv = ent + "_" + new SimpleDateFormat( "yyyyMMddHHmmss" ).format( new Date(  ) );
+        helper.getResponse().setHeader("Content-Disposition", "attachment;filename="+csv+".csv");
+        helper.getResponse().setHeader("Content-Type", "application/octet-stream;charset=UTF-8");
 
         if (lp.hasNext()) {
             Map<String, Object> ds = lp.next();
-            if (ds.isEmpty()) {
-                return;
-            }
-            Map ts = FormSet.getInstance("default").getForm("__types__");
-
             StringBuilder sf = new StringBuilder();
-            for(String fn : ds.keySet( )) {
-                String ft ;
+            StringBuilder sb = new StringBuilder();
+            Map ts = FormSet.getInstance("default").getEnum("__types__");
+
+            for(Map.Entry<String, Object> et : ds.entrySet()) {
+                Object fv = et.getValue();
+                String fn = et.getKey(  );
                 Map mt = (Map) fs.get(fn);
-                fn = ( String) mt.get("text");
-                ft = ( String) mt.get("type");
-                ft = ( String) ts.get(ft);
+                Map ex = null;
+
+                String fx, ft;
+                fx = (String) mt.get("text");
+                ft = (String) mt.get("type");
+                ft = (String) ts.get(ft);
                 if ("enum".equals(ft)) {
                     String xonf = (String) mt.get("conf");
                     String xame = (String) mt.get("enum");
                     if (null == xonf || "".equals( xonf )) xonf = mod;
                     if (null == xame || "".equals( xame )) xame = ent;
-                    es.put(fn, FormSet.getInstance(xonf).getEnum(xame));
+                    ex = FormSet.getInstance(xonf).getEnum(xame);
+                    es.put(fn, ex);
                 }
-                
-                sf.append(",\"").append(quotes(fn, es)).append("\"");
-            }
-            sf.append("\r\n");
-            ot.write(sf.substring(1));
 
-            StringBuilder sb = new StringBuilder();
-            for(Object fv : ds.values( )) {
-                sb.append(",\"").append(quotes(fv, es)).append("\"");
+                sf.append(",\"").append(escape(fx,null)).append("\"");
+                sb.append(",\"").append(escape(fv, ex )).append("\"");
             }
-            sb.append("\r\n");
-            ot.write(sb.substring(1));
+
+            ot.write(sf.append("\r\n").substring(1));
+            ot.write(sb.append("\r\n").substring(1));
         }
 
-        for(Map ds : lp ) {
+        while (lp.hasNext()) {
+            Map<String, Object> ds = lp.next();
             StringBuilder sb = new StringBuilder();
-            for(Object fv : ds.values( )) {
-                sb.append(",\"").append(quotes(fv, es)).append("\"");
+
+            for(Map.Entry<String, Object> et : ds.entrySet()) {
+                Object fv = et.getValue();
+                String fn = et.getKey(  );
+                Map ex = (Map) es.get(fn);
+
+                sb.append(",\"").append(escape(fv, ex )).append("\"");
             }
-            sb.append("\r\n");
-            ot.write(sb.substring(1));
+
+            ot.write(sb.append("\r\n").substring(1));
         }
     }
 
-    protected final String quotes(Object v, Map es) {
+    protected String escape(Object v, Map es) {
         if (v == null) {
             return "";
         }
@@ -227,7 +188,7 @@ public class DataAction extends LuceneAction {
         if (v instanceof Collection) {
             StringBuilder sb = new StringBuilder();
             for(Object x : ((Collection) v)) {
-                x = quotes(v, es);
+                x = escape(v, es);
                 sb.append( "," ).append( x );
             }
             v = sb.substring(1, sb.length());

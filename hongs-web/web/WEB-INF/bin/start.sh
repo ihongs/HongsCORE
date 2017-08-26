e9608a120094d6cfe9b109d873a0d984dafa039e
Hongs <Hongs@hongs-mba.lan>
2017-08-26 22:32:14 +0800

    完善 docker 启动代码, 使其可优雅的停止

---
+++ hongs-web/web/WEB-INF/bin/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PWD=$(cd `dirname $0`; pwd)
+CWD=`dirname $PWD`
+APP="$CWD/bin/app"
+PID="$CWD/var/server/8080.pid"
+
+# Make sure the process is stoped, and pid file is removed
+if [ -f "$PID" ]
+then
+  kill `cat "$PID"`
+  rm -f "$PID"
+  sleep 3
+fi
+
+exec "$APP" server.start --debug 2

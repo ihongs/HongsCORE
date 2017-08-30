36fd8f229cac8203a1f1ef029dd31a16542e3b89
Hongs <Hongs@hongs-mba.lan>
2017-08-30 20:19:06 +0800

    从 web.xml 的 welcome-file-list 提取索引文件名

--- hongs-serv/src/main/java/app/hongs/serv/common/SparFilter.java
+++ hongs-serv/src/main/java/app/hongs/serv/common/SparFilter.java
@@ -3,8 +3,11 @@ package app.hongs.serv.common;
 import app.hongs.Core;
 import app.hongs.action.ActionDriver;
 import app.hongs.action.ActionDriver.FilterCheck;
+import app.hongs.util.Synt;
 import java.io.File;
 import java.io.IOException;
+import java.util.Set;
+import java.util.LinkedHashSet;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -12,9 +15,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-/*
-import java.util.Set;
-import java.util.HashSet;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -22,7 +22,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-*/
 
 /**
  * 单页应用路由
@@ -31,123 +30,90 @@ import org.xml.sax.SAXException;
 public class SparFilter implements Filter {
 
     private FilterCheck ignore;
-//  private Set<String> indexs;
-//  private int         offset;
+    private Set<String> access;
 
     @Override
     public void init(FilterConfig cnf) throws ServletException {
-        this.ignore = new FilterCheck(
+        ignore = new FilterCheck(
             cnf.getInitParameter("ignore-urls"),
             cnf.getInitParameter("attend-urls")
         );
-//      this.offset = Core.BASE_PATH.length(  );
-//      this.indexs = getUrlPatterns(cnf.getFilterName());
+
+        // 索引文件列表
+        access = Synt.toSet(cnf.getInitParameter("index-file"));
+        if (access == null) {
+            access  = getWelcomeFileList();
+        }
     }
 
     @Override
     public void destroy() {
         this.ignore = null;
+        this.access = null;
     }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse rsp, FilterChain fc)
     throws IOException, ServletException {
         HttpServletRequest raq = (HttpServletRequest) req;
-        String url = ActionDriver.getCurrPath(raq);
+        String url = ActionDriver.getCurrPath ( raq );
 
         if (null != raq.getHeader("X-Requested-With")
-        || (null != ignore  &&  ignore.ignore(url)) ) {
+        ||  ignore.ignore (url) ) {
             fc.doFilter(req, rsp);
             return;
         }
-
-        // 查找当前的 URL 前缀
-        // 没必要了, 万一找不到最后就到首页了, 也就知道哪里缺少了文件了
-        /*
-        String  top = null;
-        for  ( String pre : indexs ) {
-            if (url.startsWith(pre)) {
-                top = pre ;
-            }
-        }
-        if (null == top) {
-            fc.doFilter(req, rsp);
-            return;
-        }
-        */
 
         String uri;
         File   src;
-        int    pos;
 
         // URL 总是指向索引文件
-        if (url.endsWith("/")) {
-            uri = url +  "index.html";
-            src = new File(Core.BASE_PATH + uri);
-            if (src.isFile()) {
-                fc.doFilter(req, rsp);
-                return;
-            }
-
-            /*
-            uri = url +  "index.jsp" ;
-            src = new File (Core.BASE_PATH + uri);
-            if (src.isFile()) {
-                fc.doFilter(req, rsp);
-                return;
-            }
-            */
-        } else {
+        if (! url.endsWith("/") ) {
             src = new File(Core.BASE_PATH + url);
             if (src.isFile()) {
                 fc.doFilter(req, rsp);
                 return;
             }
 
-            uri = url + "/index.html";
-            src = new File(Core.BASE_PATH + uri);
-            if (src.isFile()) {
-                fc.doFilter(req, rsp);
-                return;
+            for (String idx : access) {
+                uri = url +"/"+ idx;
+                src = new File(Core.BASE_PATH + uri);
+                if (src.isFile()) {
+                    fc.doFilter(req, rsp);
+                    return;
+                }
             }
-
-            /*
-            uri = url + "/index.jsp" ;
-            src = new File(Core.BASE_PATH + uri);
-            if (src.isFile()) {
-                fc.doFilter(req, rsp);
-                return;
+        } else {
+            for (String idx : access) {
+                uri = url   +   idx;
+                src = new File(Core.BASE_PATH + uri);
+                if (src.isFile()) {
+                    fc.doFilter(req, rsp);
+                    return;
+                }
             }
-            */
         }
 
         // 逐级向上查找索引文件
-        while ( 0 < (pos = url.lastIndexOf( "/" )) ) {
-            url = url.substring(0, pos);
+        int pos ;
+        while ( 0 < ( pos = url.lastIndexOf("/") ) ) {
+            url = url.substring( 0 , pos);
 
-            uri = url + "/index.html";
-            src = new File(Core.BASE_PATH + uri );
-            if (src.isFile( )) {
-                req.getRequestDispatcher(uri).forward(req, rsp);
-                return;
+            for (String idx : access) {
+                uri = url +"/"+ idx;
+                src = new File(Core.BASE_PATH + uri);
+                if (src.isFile()) {
+                    fc.doFilter(req, rsp);
+                    return;
+                }
             }
-
-            /*
-            uri = url + "/index.jsp" ;
-            src = new File(Core.BASE_PATH + uri );
-            if (src.isFile( )) {
-                req.getRequestDispatcher(uri).forward(req, rsp);
-                return;
-            }
-            */
         }
 
         fc.doFilter(req, rsp);
     }
 
-    /*
-    private static Set<String> getUrlPatterns(String name) throws ServletException {
-        Set<String> urls = new HashSet();
+    private static Set<String> getWelcomeFileList() throws ServletException {
+        Set<String> set = new LinkedHashSet();
 
         try {
             File xml = new File(Core.CONF_PATH + "/web.xml");
@@ -156,37 +122,29 @@ public class SparFilter implements Filter {
             }
 
             DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
-            DocumentBuilder        bui = fac.newDocumentBuilder( );
+            DocumentBuilder        bui = fac.newDocumentBuilder();
             Document               doc = bui.parse(xml);
 
-            NodeList lst = doc.getElementsByTagName("filter-name");
+            NodeList lst = doc.getElementsByTagName("welcome-file-list");
             for (int i = 0; i < lst.getLength(); i ++ ) {
-                Element nod = (Element) lst.item(i);
-                if (!nod.getTextContent().equals(name)) {
-                    continue;
-                }
-
-                NodeList lzt = nod.getParentNode().getChildNodes();
-                for (int j = 0; j < lzt.getLength(); j ++ ) {
-                    Element ele = (Element) lzt.item(i);
-                    if (!nod.getTagName(  ).equals("url-pattern")) {
-                        continue;
-                    }
-
-                    // 去掉后缀 *
-                    urls.add(ele.getTextContent().replace("*",""));
+                Element ele = (Element) lst.item(i);
+                if (ele.getTextContent( ).equals("welcome-file")) {
+                    set.add ( ele.getTextContent());
                 }
             }
-        } catch (ParserConfigurationException ex) {
+        } catch (ParserConfigurationException ex ) {
             throw new ServletException(ex);
         } catch (SAXException ex) {
             throw new ServletException(ex);
-        } catch (IOException ex) {
+        } catch ( IOException ex) {
             throw new ServletException(ex);
         }
 
-        return urls;
+        if (set.isEmpty()) {
+            set.add("index.html");
+        }
+
+        return set;
     }
-    */
 
 }

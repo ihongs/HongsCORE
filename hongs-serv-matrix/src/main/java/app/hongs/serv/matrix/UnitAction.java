5a131f730ecd61afc9d8f71c989cfb64166b8307
Hongs <Hongs@hongs-mba.lan>
2017-08-30 20:17:58 +0800

    修正生成菜单时权重排序未起作用的问题

--- hongs-serv-matrix/src/main/java/app/hongs/serv/matrix/UnitAction.java
+++ hongs-serv-matrix/src/main/java/app/hongs/serv/matrix/UnitAction.java
@@ -4,9 +4,9 @@ import app.hongs.CoreLocale;
 import app.hongs.HongsException;
 import app.hongs.action.ActionHelper;
 import app.hongs.action.anno.Action;
-import app.hongs.db.DB;
 import app.hongs.action.anno.CommitSuccess;
 import app.hongs.action.anno.Verify;
+import app.hongs.db.DB;
 import java.util.HashMap;
 import java.util.Map;
 

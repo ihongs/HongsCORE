5a36a70d90e471cade900ca99ded207e52808bfd
Hongs <ihongs@live.cn>
2017-11-11 10:41:08 +0800

    改包名

---
+++ hongs-serv/src/main/java/app/hongs/common/serv/BaseConfig.java
@@ -0,0 +1,50 @@
+package app.hongs.common.serv;
+
+import app.hongs.action.ActionDriver;
+import app.hongs.action.serv.AuthAction;
+import app.hongs.action.serv.ConfAction;
+import app.hongs.action.serv.LangAction;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 通用基础外部配置接口
+ * 将 ConfAction, LangAction, AuthAction 集合到一起
+ * @author Hongs
+ */
+public class BaseConfig extends ActionDriver {
+
+    ConfAction conf = new ConfAction();
+    LangAction lang = new LangAction();
+    AuthAction auth = new AuthAction();
+
+    @Override
+    public void service(HttpServletRequest req, HttpServletResponse rsp)
+      throws ServletException, IOException
+    {
+        String name = req.getServletPath();
+               name = name.substring(name.lastIndexOf("/"));
+        if ("/conf".equals( name )) {
+            conf.service(req, rsp);
+        } else
+        if ("/lang".equals( name )) {
+            lang.service(req, rsp);
+        } else
+        if ("/auth".equals( name )) {
+            auth.service(req, rsp);
+        } else
+        {
+            rsp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            rsp.getWriter().print("Unsupported name "+name);
+        }
+    }
+
+    @Override
+    public void destroy()
+    {
+        conf.destroy();
+        lang.destroy();
+    }
+}
---
+++ hongs-serv/src/main/java/app/hongs/common/serv/SessFilter.java
@@ -0,0 +1,105 @@
+package app.hongs.common.serv;
+
+import app.hongs.Core;
+import app.hongs.HongsException;
+import app.hongs.action.ActionDriver;
+import app.hongs.action.ActionDriver.FilterCheck;
+import app.hongs.util.Synt;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 会话状态过滤
+ * @author Hongs
+ */
+public class SessFilter implements Filter {
+
+    protected String SSRN = "SSID"; // 会话请求参数名称
+    protected String SSCN = "SSID"; // 会话 Cookie 键名
+    protected String SSCP =     ""; // 会话 Cookie 路径
+    protected int    SEXP =  86400; // 会话过期时间(秒)
+    protected int    CEXP =     -1; // 过期 Cookie (秒)
+
+    private String      inside = null; // 过滤器标识
+    private FilterCheck ignore = null; // 待忽略用例
+
+    @Override
+    public void init(FilterConfig fc)
+    throws ServletException {
+        String fn;
+
+        fn = fc.getInitParameter("request-name");
+        if (fn != null) SSRN = fn;
+
+        fn = fc.getInitParameter( "cookie-name");
+        if (fn != null) SSCN = fn;
+
+        fn = fc.getInitParameter( "cookie-path");
+        if (fn != null) SSCP = fn;
+
+        fn = fc.getInitParameter("session-max-age");
+        if (fn != null) SEXP = Integer.parseInt(fn);
+
+        fn = fc.getInitParameter( "cookie-max-age");
+        if (fn != null) CEXP = Integer.parseInt(fn);
+
+        if (! SSCP.startsWith("/")) {
+            SSCP = Core.BASE_HREF + "/" + SSCP;
+        }
+
+        inside = SessFilter.class.getName()+":"+fc.getFilterName()+":INSIDE";
+        ignore = new FilterCheck(
+            fc.getInitParameter("ignore-urls"),
+            fc.getInitParameter("attend-urls")
+        );
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse rsp, FilterChain flt)
+    throws ServletException, IOException {
+        HttpServletRequest  raq = (HttpServletRequest ) req;
+        HttpServletResponse rzp = (HttpServletResponse) rsp;
+
+        /**
+         * 对于嵌套相同过滤, 不在内部重复执行;
+         * 如外部设置了忽略, 则跳过忽略的路径.
+         */
+        if (inside != null &&  Synt.declare(raq.getAttribute(inside),false)
+        ||  ignore != null && ignore.ignore(ActionDriver.getCurrPath(raq))) {
+            flt.doFilter(req, rsp);
+            return;
+        }
+
+        try {
+            raq = new SessAccess(raq, rzp, this);
+            raq.setAttribute    ( inside , true);
+            flt.doFilter        (raq, rzp);
+        } finally {
+            raq.removeAttribute ( inside );
+
+            // 最终任务完成后对会话进行保存
+            HttpSession ses  = raq.getSession (false);
+            if (null != ses && ses instanceof Sesion) {
+                try {
+                    ( (Sesion) ses).close();
+                } catch (HongsException ex) {
+                    throw new ServletException(ex);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+        // Nothing todo.
+    }
+
+}

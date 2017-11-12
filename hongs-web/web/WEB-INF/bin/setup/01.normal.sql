2fa8115ee7b89be431035fdfa45557d602a3b690
Hongs <Hongs@hongs-mba.lan>
2017-11-12 11:39:19 +0800

    common 更名为 normal

---
+++ hongs-web/web/WEB-INF/bin/setup/01.normal.sql
@@ -0,0 +1,33 @@
+-- DB: normal
+
+--
+-- 简单键值存储
+--
+
+DROP TABLE IF EXISTS `a_normal_record`;
+CREATE TABLE `a_normal_record` (
+    `id` VARCHAR(96) NOT NULL,
+    `data`  BLOB NOT NULL,
+    `xtime` UNSIGNED INT(11) DEFAULT 0,
+    `mtime` UNSIGNED INT(11) DEFAULT 0,
+    PRIMARY KEY (`id`)
+);
+
+CREATE INDEX `IK_a_normal_record_xtime` ON `a_normal_record` (`xtime`);
+CREATE INDEX `IK_a_normal_record_mtime` ON `a_normal_record` (`mtime`);
+
+--
+-- 会话数据存储
+--
+
+DROP TABLE IF EXISTS `a_normal_sesion`;
+CREATE TABLE `a_normal_sesion` (
+    `id` VARCHAR(96) NOT NULL,
+    `data`  BLOB NOT NULL,
+    `xtime` UNSIGNED INT(11) DEFAULT 0,
+    `mtime` UNSIGNED INT(11) DEFAULT 0,
+    PRIMARY KEY (`id`)
+);
+
+CREATE INDEX `IK_a_normal_sesion_xtime` ON `a_normal_sesion` (`xtime`);
+CREATE INDEX `IK_a_normal_sesion_mtime` ON `a_normal_sesion` (`mtime`);

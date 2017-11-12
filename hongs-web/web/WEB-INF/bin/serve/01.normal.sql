5b63e2d6cf0bb2b3ad70ed9cdce490d5de45a0aa
Hongs <ihongs@live.cn>
2017-11-12 11:40:51 +0800

    common 更名为 normal

--- hongs-web/web/WEB-INF/bin/serve/01.normal.sql
+++ hongs-web/web/WEB-INF/bin/serve/01.normal.sql
@@ -1,6 +1,6 @@
--- DB: common
+-- DB: normal
 -- DT: -1D
 
-DELETE FROM a_common_record WHERE xtime < '{{yyyy/MM/dd}}';
+DELETE FROM a_normal_record WHERE xtime < '{{yyyy/MM/dd}}';
 
-DELETE FROM a_common_sesion WHERE xtime < '{{yyyy/MM/dd}}';
+DELETE FROM a_normal_sesion WHERE xtime < '{{yyyy/MM/dd}}';

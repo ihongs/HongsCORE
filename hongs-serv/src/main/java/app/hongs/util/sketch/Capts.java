543d4e2884ee3883bb6a4d38712985b6d56d7c99
Hongs <ihongs@live.cn>
2017-12-23 08:37:02 +0800

    微调验证码

--- hongs-serv/src/main/java/app/hongs/util/sketch/Capts.java
+++ hongs-serv/src/main/java/app/hongs/util/sketch/Capts.java
@@ -25,18 +25,18 @@ import javax.imageio.ImageIO;
  */
 public class Capts {
 
-    private int width = 160;
+    private int width = 100;
     private int height = 40;
-    private int codeCount = 4 ;
-    private int maskCount = 17;
+    private int codeCount = 4;
+    private int maskCount = 8;
     private float fontRatio = 0.80f;
-    private float mendRatio = 0.167f;
-    private float maskRatio = 0.025f;
+    private float mendRatio = 0.10f;
+    private float maskRatio = 0.05f;
     private Color backColor = Color.WHITE;
     private Color fontColor = Color.BLACK;
-    private String fontFile ="!Anja Eliane.ttf";
+    private String fontFile ="!Capts.ttf";
     private char[] fontDict = new char[] {
-        '1', '3', '4', '5', '6', '7', '8', '9',
+        '3', '4', '5', '6', '7', '8', '9',
         'A', 'B', 'C', 'D', 'E', 'F', 'G',
         'H', 'I', 'J', 'K', 'L', 'M', 'N',
         'P', 'Q', 'R', 'S', 'T',
@@ -109,14 +109,14 @@ public class Capts {
         int fs = (int) ((float) height * fontRatio); // 最大字号
         int fz = (int) ((float)   fs   *   0.80f  ); // 最小字号
 
-        Random        rd = new  Random( );
+        Random        rd = new  Random();
         StringBuilder sb = new  StringBuilder ();
         Graphics2D    gd = buff.createGraphics();
 
         gd.setColor(backColor);
-        gd.fillRect(0, 0, width, height );
+        gd.fillRect(0, 0, width, height);
         gd.setColor(fontColor);
-        gd.setStroke(new BasicStroke(ls));
+        gd.setStroke(new BasicStroke(ls, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
         gd.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         Font font;
@@ -136,10 +136,10 @@ public class Capts {
 
         for(int i  = 0; i < codeCount; i ++) {
             int fn = rd.nextInt ( fs - fz  ) + fz  ;
-            int fe = (int) ((float) fn * mendRatio); // 误差修正
+            int fe = (  int  )  ( fn * mendRatio  ); // 误差修正, 避免字超大出界!
             int j  = rd.nextInt ( fontDict.length );
             String ss = String.valueOf(fontDict[j]);
-            sb.append(ss );
+            sb.append(ss);
             gd.setFont(font.deriveFont(Font.CENTER_BASELINE , fn) );
             int fx = (fw - gd.getFontMetrics().stringWidth(ss)) / 2;
             gd.drawString(ss, fw / 2 + fw * i + fx, (height - fn) / 2 + fn - fe);
@@ -177,8 +177,9 @@ public class Capts {
     }
 
     private void shearX(int w, int h, Graphics g, Random r, Color c) {
-        int period = r.nextInt((int) ((float) h * 0.2f));
-        int phases = r.nextInt((int) ((float) h * 0.2f));
+        int x  = (int) (0.2f * h);
+        int period = r.nextInt(x)+ x;
+        int phases = r.nextInt(x)+ x;
         int frames = r.nextInt(w - 2) + 2;
 
         for (int i = 0; i < h; i ++) {
@@ -194,8 +195,9 @@ public class Capts {
     }
 
     private void shearY(int w, int h, Graphics g, Random r, Color c) {
-        int period = r.nextInt((int) ((float) h * 0.2f));
-        int phases = r.nextInt((int) ((float) h * 0.2f));
+        int x  = (int) (0.2f * h);
+        int period = r.nextInt(x)+ x;
+        int phases = r.nextInt(x)+ x;
         int frames = r.nextInt(w - 2) + 2;
 
         for (int i = 0; i < w; i ++) {
@@ -211,20 +213,20 @@ public class Capts {
     }
 
     public static Capts captcha(int h, String b, String f, String e) {
-        if (h < 40 || h > 200) {
-            throw new HongsExpedient.Common("h must be 40 ~ 200 px");
+        if (h < 24 || h > 96) {
+            throw new HongsExpedient(0x1100, "h must be 24~96 (px)");
         }
 
         // 获取配置
         CoreConfig  cc = CoreConfig.getInstance();
-        String ff = cc.getProperty("core.capts.font.file", "!Anja Eliane.ttf");
-        String cs = cc.getProperty("core.capts.code.dict", "13456789ABCDEFGHIJKLMNPQRSTUVWXY");
-        int    cn = cc.getProperty("core.capts.code.count", 4 );
-        int    mn = cc.getProperty("core.capts.mask.count", 17);
-        float  sr = cc.getProperty("core.capts.size.ratio", 0.6f);
-        float  fr = cc.getProperty("core.capts.font.ratio", 0.8f);
-        float  mr = cc.getProperty("core.capts.mend.ratio", 0.167f);
-        float  xr = cc.getProperty("core.capts.mask.ratio", 0.025f);
+        String ff = cc.getProperty("core.capts.font.file", "!Capts.ttf");
+        String cs = cc.getProperty("core.capts.code.dict", "3456789ABCDEFGHJKMNPQRSTUVWXY");
+        int    cn = cc.getProperty("core.capts.code.count", 4);
+        int    mn = cc.getProperty("core.capts.mask.count", 8);
+        float  sr = cc.getProperty("core.capts.size.ratio", 0.40f);
+        float  fr = cc.getProperty("core.capts.font.ratio", 0.80f);
+        float  mr = cc.getProperty("core.capts.mend.ratio", 0.10f);
+        float  xr = cc.getProperty("core.capts.mask.ratio", 0.05f);
         int    w  = (int) ((float) h * sr * (cn + 1));
 
         char[] cd = cs.toCharArray();

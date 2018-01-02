f28f763f5d3b43f83b48043148616ad7180bcd03
Hongs <Hongs@hongs-mba.lan>
2018-01-03 00:27:46 +0800

    微调 tagsinput, 使其更好的融入环境

--- hongs-wms/web/static/assets/src/hongscore-tagsinput.js
+++ hongs-wms/web/static/assets/src/hongscore-tagsinput.js
@@ -13,11 +13,36 @@
             if ($(this).data("tagsinput")) {
                 return;
             }
-            
-            /**/$(this).tagsinput();
-            if ($(this).data("fl")) {
-                $(this).data("fl", "$.isArray(v) ? v.join(',') : v");
-            }
+
+            $(this).tagsinput({
+                trimValue  : true,
+                confirmKeys: [ 13,44,59, 0xff0c,0xff1b ]
+            });
+
+            // 微调样式以融入环境
+            // 并阻止回车导致提交
+            $(this).siblings ( ".bootstrap-tagsinput"  )
+                   .attr("class", $(this).attr("class"))
+                   .addClass (  "bootstrap-tagsinput"  )
+                   .attr("style", $(this).attr("style"))
+                   .show()
+                   .on("keypress",function(e) {
+                        if (e.keyCode === 13) {
+                            e.stopPropagation();
+                            return false;
+                        }
+                    });
+
+            // 表单项改变时需重建
+            $(this).on( "change" ,function(e) {
+                var ti = $(this).data("tagsinput");
+                $('.tag' , ti.$container).remove();
+                $('option' , ti.$element).remove();
+                while (ti.itemsArray.length  >  0) {
+                    ti.itemsArray.pop(  );
+                }
+                ti.add($(this).val(), !0);
+            });
         });
     });
 })(jQuery);

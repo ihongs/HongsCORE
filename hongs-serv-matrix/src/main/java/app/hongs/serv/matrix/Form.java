cdca13d7e8ab1b31e04e09daab0b262454530a5c
Hongs <Hongs@hongs-mba.lan>
2018-02-01 00:11:18 +0800

    修复 id 为 hidden 是导致其为 stored 类型而无法筛选的问题

--- hongs-serv-matrix/src/main/java/app/hongs/serv/matrix/Form.java
+++ hongs-serv-matrix/src/main/java/app/hongs/serv/matrix/Form.java
@@ -596,17 +596,13 @@ public class Form extends Model {
                 }
             } else
             // 可搜索指定存为搜索类型
-            if (Synt.declare(fiel.get("srchable"), false) ) {
+            if (Synt.declare(fiel.get("srchable") , false)) {
                 if(!fiel.containsKey("lucnene-type")) {
                     fiel.put("lucene-type", "search");
                 }
             } else
             // 文本禁搜索则为存储类型
-            // 字串还需不可筛选和排序
-            if ("textarea".equals( t )
-            || ("string"  .equals( types.get(t) )
-            && !Synt.declare(fiel.get("fitrable"), false)
-            && !Synt.declare(fiel.get("sortable"), false))) {
+            if ("stored".equals(t) || "textarea".equals(t)) {
                 if(!fiel.containsKey("lucnene-type")) {
                     fiel.put("lucene-type", "stored");
                 }

664d5184859088c642ec4bfbe9ce124e5147c1e7
Hongs <ihongs@live.cn>
2018-02-24 21:16:48 +0800

    Malleable 更名为 JoistBean, 增加 JointGate

--- hongs-serv-search/src/main/java/app/hongs/dh/lucene/LuceneRecord.java
+++ hongs-serv-search/src/main/java/app/hongs/dh/lucene/LuceneRecord.java
@@ -9,7 +9,7 @@ import app.hongs.HongsExpedient;
 import app.hongs.action.FormSet;
 import app.hongs.dh.IEntity;
 import app.hongs.dh.ITrnsct;
-import app.hongs.dh.Malleable;
+import app.hongs.dh.JoistBean;
 import app.hongs.dh.lucene.field.*;
 import app.hongs.dh.lucene.query.*;
 import app.hongs.dh.lucene.value.*;
@@ -71,7 +71,7 @@ import org.apache.lucene.store.FSDirectory;
  *
  * @author Hongs
  */
-public class LuceneRecord extends Malleable implements IEntity, ITrnsct, Cloneable, AutoCloseable {
+public class LuceneRecord extends JoistBean implements IEntity, ITrnsct, Cloneable, AutoCloseable {
 
     protected boolean TRNSCT_MODE = false;
     protected boolean OBJECT_MODE = false;

167ae3ddeb34747cb410de9224afac667bd130b4
Hongs <ihongs@live.cn>
2018-03-28 22:57:01 +0800

    增加 bool 词汇

--- hongs-core/src/main/java/app/hongs/util/Synt.java
+++ hongs-core/src/main/java/app/hongs/util/Synt.java
@@ -64,12 +64,12 @@ public final class Synt {
     /**
      * 视为假的字符串有: false,no,f,n,0 和 空字符串
      */
-    public  static final Pattern FAKE = Pattern.compile("^(false|no|f|n|0|)$", Pattern.CASE_INSENSITIVE);
+    public  static final Pattern FAKE = Pattern.compile("^(FALSE|OFF|NO|F|N|O|0|)$", Pattern.CASE_INSENSITIVE);
 
     /**
      * 视为真的字符串有: true,yes,t,y,1
      */
-    public  static final Pattern TRUE = Pattern.compile("^(true|yes|t|y|1)$" , Pattern.CASE_INSENSITIVE);
+    public  static final Pattern TRUE = Pattern.compile( "^(TRUE|YES|ON|T|Y|I|1)$" , Pattern.CASE_INSENSITIVE);
 
     /**
      * 快速构建 List

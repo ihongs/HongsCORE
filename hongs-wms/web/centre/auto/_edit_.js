c5e9925924edf11d0e1f052f8082eb22cb3c9fbd
Hongs <Hongs@hongs-mba.lan>
2018-04-20 23:59:19 +0800

    修改上传路径

--- hongs-wms/web/centre/auto/_edit_.js
+++ hongs-wms/web/centre/auto/_edit_.js
@@ -149,7 +149,7 @@ function setEditor(node, func) {
                             data.append("file.",files[i]);
                         }
                         $.ajax({
-                            url     : hsFixUri("centre/medium/upload/image/create.act"),
+                            url     : hsFixUri("centre/upload/image.jsp"),
                             data    :  data ,
                             type    : "POST",
                             dataType: "json",

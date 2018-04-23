1bf6687abbfc16fae3daf86c5bf397400a83b350
Hongs <Hongs@hongs-mba.lan>
2018-04-23 20:10:30 +0800

    修改了已删除的数据结构

--- hongs-wms/web/WEB-INF/bin/serve/03.matrix.sql
+++ hongs-wms/web/WEB-INF/bin/serve/03.matrix.sql
@@ -1,4 +1,4 @@
 -- DB: module
 -- DT: -1M
 
-DELETE FROM `a_matrix_data` WHERE `state` = 0 AND `etime` < '{{yyyy/MM/dd}}';
+DELETE FROM `a_matrix_data` WHERE (`state` = 1 AND `etime` < '{{yyyy/MM/dd}}') OR (`state` = 0 `ctime` < '{{yyyy/MM/dd}}');

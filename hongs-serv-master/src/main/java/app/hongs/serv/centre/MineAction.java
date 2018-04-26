994a30c80333c1ca7d7ab1198ca5c2376a8b83a5
Hongs <Hongs@hongs-mba.lan>
2018-04-26 21:58:49 +0800

    去除已废弃的配置

--- hongs-serv-master/src/main/java/app/hongs/serv/centre/MineAction.java
+++ hongs-serv-master/src/main/java/app/hongs/serv/centre/MineAction.java
@@ -1,7 +1,6 @@
 package app.hongs.serv.centre;
 
 import app.hongs.Cnst;
-import app.hongs.CoreConfig;
 import app.hongs.action.ActionHelper;
 import app.hongs.action.anno.Action;
 import java.util.HashMap;
@@ -16,12 +15,11 @@ public class MineAction extends app.hongs.serv.centra.MineAction {
 
     @Action("sign/info")
     public void signInfo(ActionHelper ah) {
-        Map        sd = new HashMap();
-        String     sk = CoreConfig.getInstance().getProperty("core.api.ssid");
-        sd.put(sk, ah.getRequest().getRequestedSessionId());
+        Map sd = new HashMap();
         sd.put(Cnst.UID_SES, ah.getSessibute(Cnst.UID_SES));
         sd.put(Cnst.STM_SES, ah.getSessibute(Cnst.STM_SES));
         sd.put(Cnst.SAE_SES, ah.getSessibute(Cnst.SAE_SES));
+        sd.put("sesid", ah.getRequest().getRequestedSessionId());
         sd.put("appid", ah.getSessibute("appid"));
         sd.put("uname", ah.getSessibute("uname"));
         sd.put("uhead", ah.getSessibute("uhead"));

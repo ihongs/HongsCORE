c1097074431719c39efd3b484518bba6c478ff60
Hongs <ihongs@live.cn>
2018-05-04 22:44:33 +0800

    增加预置外部数据支持

--- hongs-wms/web/centra/matrix/util.js
+++ hongs-wms/web/centra/matrix/util.js
@@ -74,9 +74,9 @@ function getFormInfo(id, func) {
 
 var COLS_PATT_BASE = /^[a-z0-9\-_.:]+$/;    // 当作为标签属性时可以使用, 没有引起问题的特殊字符
 var COLS_PATT_DATA = /^data-/;              // 以 data 开头不用再加前缀, 如 data-tk, data-ak 等
-var COLS_PATT_DATL = /^data-.{3,}/;         // 长属性在写回时要去掉前缀
-var COLS_PATT_DATN = /^data-\d+?$/;         // 数字结尾要从属性值里取键
-var COLS_PATT_DATS = /^data-(?!fn|ft)/;     // 这两个很特殊需要特殊处理
+var COLS_PATT_DATN = /^data-\d+$/;          // 数字结尾要从属性值里取键
+var COLS_PATT_DATL = /^data-(?=\W|...)/;    // 长属性在写回时要去掉前缀
+var COLS_PATT_DATS = /^data-(?!fn|ft?)/;    // 这两个很特殊需要特殊处理
 
 /**
  * 打开设置时载入字段配置

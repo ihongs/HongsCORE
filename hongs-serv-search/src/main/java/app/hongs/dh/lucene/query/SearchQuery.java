65db0da5a58446236dd915a91cef6e4bcfb52343
Hongs <Hongs@fe80:0:0:0:142f:3dff:feb8:da10%8>
2018-06-21 19:53:04 +0800

    模糊匹配分词间关系改默认的或为与

--- hongs-serv-search/src/main/java/app/hongs/dh/lucene/query/SearchQuery.java
+++ hongs-serv-search/src/main/java/app/hongs/dh/lucene/query/SearchQuery.java
@@ -13,32 +13,25 @@ import org.apache.lucene.search.Query;
  * @author Hongs
  */
 public class SearchQuery implements IQuery {
+    private Analyzer ana = null;
     private Boolean  des = null;
-    private Boolean  and = null;
-    private Boolean  alw = null;
-    private Boolean  let = null;
-    private Boolean  epi = null;
+    private Boolean  dor = null;
     private Integer  phr = null;
     private Integer  fpl = null;
     private Float    fms = null;
-    private Analyzer ana = null;
+    private Boolean  art = null;
+    private Boolean  alw = null;
+    private Boolean  let = null;
+    private Boolean  epi = null;
+    private Boolean  agp = null;
     public void  analyzer(Analyzer a) {
         this.ana = a;
     }
-    public void  advanceAnalysisInUse(Boolean x) {
+    public void  smartParse (Boolean x) {
         this.des = x;
     }
-    public void  defaultOperatorIsAnd(Boolean x) {
-        this.and = x;
-    }
-    public void  allowLeadingWildcard(Boolean x) {
-        this.alw = x;
-    }
-    public void  lowercaseExpandedTerms(Boolean x) {
-        this.let = x;
-    }
-    public void  enablePositionIncrements(Boolean x) {
-        this.epi = x;
+    public void  lightMatch (Boolean x) {
+        this.dor = x;
     }
     public void  phraseSlop (Integer x) {
         this.phr = x;
@@ -49,25 +42,49 @@ public class SearchQuery implements IQuery {
     public void  fuzzyMinSim(Float   x) {
         this.fms = x;
     }
+    public void  analyzeRangeTerms(Boolean x) {
+        this.art = x;
+    }
+    public void  allowLeadingWildcard(Boolean x) {
+        this.alw = x;
+    }
+    public void  lowercaseExpandedTerms(Boolean x) {
+        this.let = x;
+    }
+    public void  enablePositionIncrements(Boolean x) {
+        this.epi = x;
+    }
+    public void  autoGeneratePhraseQueries(Boolean x) {
+        this.agp = x;
+    }
 
     @Override
     public Query get(String k, Object v) {
         try {
             QueryParser qp = new QueryParser(k, ana != null ? ana : new StandardAnalyzer());
 
-            String s = v.toString ( );
-            if (des == null || !des ) {
-                s = QueryParser.escape(s);
+            // 是否转义
+            String s = v.toString( );
+            if (des == null || !des) {
+                s = QueryParser.escape (s);
             }
-            if (and != null &&  and ) {
-                qp.setDefaultOperator (QueryParser.AND_OPERATOR);
+
+            // 词间关系
+            if (dor == null || !dor) {
+                qp.setDefaultOperator(QueryParser.AND_OPERATOR);
+            } else {
+                qp.setDefaultOperator(QueryParser. OR_OPERATOR);
             }
-            if (epi != null) qp.setEnablePositionIncrements(epi);
-            if (let != null) qp.setLowercaseExpandedTerms(let);
-            if (alw != null) qp.setAllowLeadingWildcard(alw);
-            if (fpl != null) qp.setFuzzyPrefixLength(fpl);
-            if (fms != null) qp.setFuzzyMinSim      (fms);
+
+            // 其他设置
             if (phr != null) qp.setPhraseSlop       (phr);
+            if (fms != null) qp.setFuzzyMinSim      (fms);
+            if (fpl != null) qp.setFuzzyPrefixLength(fpl);
+            if (art != null) qp.setAnalyzeRangeTerms(art);
+            if (alw != null) qp.setAllowLeadingWildcard     (alw);
+            if (let != null) qp.setLowercaseExpandedTerms   (let);
+            if (epi != null) qp.setEnablePositionIncrements (epi);
+            if (agp != null) qp.setAutoGeneratePhraseQueries(agp);
 
             Query  q2 = qp.parse(s);
             return q2 ;

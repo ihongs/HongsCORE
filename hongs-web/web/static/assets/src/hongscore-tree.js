e77cc20c8fdd1f9f7e55bdeea0ecc374a3f70533
Hongs <ihongs@live.cn>
2018-08-05 14:14:32 +0800

    修复树删除节点问题

--- hongs-web/web/static/assets/src/hongscore-tree.js
+++ hongs-web/web/static/assets/src/hongscore-tree.js
@@ -350,7 +350,7 @@ HsTree.prototype = {
         var that = this;
         var func = function() {
         var dat2 = jQuery.extend({}, hsSerialDat(url), hsSerialDat(data||{}));
-        this.ajax({
+        that.ajax({
             "url"       : url ,
             "data"      : data,
             "type"      : "POST",

db500faafe7c779a0297bee1f0e47d2f56a32b5c
Hongs <Hongs@fe80:0:0:0:142f:3dff:feb8:da10%8>
2018-08-08 01:13:11 +0800

    重新编写数据写锁相关代码

--- hongs-serv-search/src/main/java/io/github/ihongs/dh/search/SearchEntity.java
+++ hongs-serv-search/src/main/java/io/github/ihongs/dh/search/SearchEntity.java
@@ -2,19 +2,28 @@ package io.github.ihongs.dh.search;
 
 import io.github.ihongs.Cnst;
 import io.github.ihongs.Core;
+import io.github.ihongs.CoreLogger;
 import io.github.ihongs.HongsException;
 import io.github.ihongs.HongsExemption;
 import io.github.ihongs.action.FormSet;
 import io.github.ihongs.dh.lucene.LuceneRecord;
+import io.github.ihongs.util.Synt;
 import io.github.ihongs.util.thread.Block;
+import io.github.ihongs.util.thread.Block.Locker;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 
 /**
  * 搜索记录
@@ -26,14 +35,14 @@ import org.apache.lucene.index.Term;
  */
 public class SearchEntity extends LuceneRecord {
 
-    private static final Map<String, IndexWriter> WRITERS = new HashMap();
+    private  SearchWriter WRITOR = null;
 
     public SearchEntity(Map form, String path, String name) {
-        super(form, path, name);
+        super(form , path , name);
     }
 
     public SearchEntity(Map form) {
-        this (form, null, null);
+        this (form , null , null);
     }
 
     /**
@@ -77,58 +86,142 @@ public class SearchEntity extends LuceneRecord {
 
     @Override
     public IndexWriter getWriter() throws HongsException {
-        final String kn = SearchEntity.class.getName() + ":" + getDbName();
-        IndexWriter  iw = WRITERS.get (kn);
-        if (iw == null) {
-            iw = super.getWriter();
-            WRITERS.put ( kn, iw );
-        }
-        return iw;
-    }
-
-    @Override
-    public void addDoc(final Document doc) throws HongsException {
-        final String kn = SearchEntity.class.getName() + ":" + getDbName();
-        Block.Locker lk = Block.getLocker(kn);
+        String dn = /***/ getDbName(  );
+        String kn = SearchWriter.class.getName() + ":" + dn ;
+        Locker lk = Block.getLocker(kn);
         lk.lock();
         try {
-            IndexWriter iw = getWriter ( );
-            iw.addDocument (doc);
-            iw.commit();
-        } catch (IOException ex) {
-            throw new HongsExemption.Common(ex);
+            if (WRITOR != null) {
+//              WRITOR.open(  ); // 不计数
+                return  WRITOR.conn();
+            }
+            WRITOR = (SearchWriter) Core.GLOBAL_CORE.get(kn);
+            if (WRITOR != null) {
+                WRITOR.open(  ); // 需计数
+                return  WRITOR.conn();
+            }
+
+            String path = getDbPath();
+            IndexWriter writer;
+
+            try {
+                IndexWriterConfig iwc = new IndexWriterConfig(getAnalyzer());
+                iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+
+                Directory dir = FSDirectory.open(Paths.get(path));
+
+                writer = new IndexWriter(dir, iwc);
+            } catch (IOException x) {
+                throw new HongsException.Common(x);
+            }
+
+            WRITOR = new SearchWriter(writer , dn);
+            Core . GLOBAL_CORE . put (kn , WRITOR);
+
+            return WRITOR.conn();
         } finally {
             lk.unlock();
         }
     }
 
     @Override
-    public void setDoc(final String id, final Document doc) throws HongsException {
-        final String kn = SearchEntity.class.getName() + ":" + getDbName();
-        Block.Locker lk = Block.getLocker(kn);
-        lk.lock();
+    public void addDoc(Document doc) throws HongsException {
+        Locker lk = lock();
         try {
-            IndexWriter iw = getWriter ( );
-            iw.updateDocument (new Term(Cnst.ID_KEY, id), doc);
-            iw.commit();
-        } catch (IOException ex) {
-            throw new HongsExemption.Common(ex);
+            IndexWriter iw = getWriter();
+            try {
+                iw.addDocument (doc);
+            } catch (IOException ex) {
+                throw new HongsException.Common(ex);
+            }
+        } finally {
+            lk.unlock();
+        }
+
+        if (!TRNSCT_MODE) {
+            commit();
+        }
+    }
+
+    @Override
+    public void setDoc(String id, Document doc) throws HongsException {
+        Locker lk = lock();
+        try {
+            IndexWriter iw = getWriter();
+            try {
+                iw.updateDocument (new Term(Cnst.ID_KEY, id), doc);
+            } catch (IOException ex) {
+                throw new HongsException.Common(ex);
+            }
+        } finally {
+            lk.unlock();
+        }
+
+        if (!TRNSCT_MODE) {
+            commit();
+        }
+    }
+
+    @Override
+    public void delDoc(String id) throws HongsException {
+        Locker lk = lock();
+        try {
+            IndexWriter iw = getWriter();
+            try {
+                iw.deleteDocuments(new Term(Cnst.ID_KEY, id) /**/);
+            } catch (IOException ex) {
+                throw new HongsException.Common(ex);
+            }
+        } finally {
+            lk.unlock();
+        }
+
+        if (!TRNSCT_MODE) {
+            commit();
+        }
+    }
+
+    /**
+     * 提交更改
+     */
+    @Override
+    public void commit() {
+        if (WRITOR == null) {
+            return;
+        }
+        TRNSCT_MODE = Synt.declare(Core.getInstance().got(Cnst.TRNSCT_MODE), false);
+
+        Locker lk = lock();
+        try {
+            IndexWriter iw = WRITOR.conn();
+            try {
+                iw.commit(  );
+            } catch (IOException ex) {
+                throw new HongsExemption(0x102c, ex);
+            }
         } finally {
             lk.unlock();
         }
     }
 
+    /**
+     * 回滚操作
+     */
     @Override
-    public void delDoc(final String id) throws HongsException {
-        final String kn = SearchEntity.class.getName() + ":" + getDbName();
-        Block.Locker lk = Block.getLocker(kn);
-        lk.lock();
+    public void revert() {
+        if (WRITOR == null) {
+            return;
+        }
+        TRNSCT_MODE = Synt.declare(Core.getInstance().got(Cnst.TRNSCT_MODE), false);
+
+        Locker lk = lock();
         try {
-            IndexWriter iw = getWriter ( );
-            iw.deleteDocuments(new Term(Cnst.ID_KEY, id) /**/);
-            iw.commit();
-        } catch (IOException ex) {
-            throw new HongsExemption.Common(ex);
+            IndexWriter iw = WRITOR.conn();
+            try {
+                iw.rollback();
+            } catch (IOException ex) {
+                throw new HongsExemption(0x102d, ex);
+            }
         } finally {
             lk.unlock();
         }
@@ -136,9 +229,131 @@ public class SearchEntity extends LuceneRecord {
 
     @Override
     public void close() {
-        final String kn = SearchEntity.class.getName() + ":" + getDbName();
-        super . close();
-        WRITERS.remove(kn);
+        super.close();
+
+        if (WRITOR == null) {
+            return;
+        }
+
+        // 默认退出时提交
+        if (TRNSCT_MODE) {
+            try {
+            try {
+                commit();
+            } catch (Error er ) {
+                revert();
+                throw er;
+            }
+            } catch (Error er ) {
+                CoreLogger.error(er);
+            }
+        }
+
+        Locker lk = lock();
+        try {
+            WRITOR.exit( );
+            WRITOR = null ;
+        } finally {
+            lk.unlock();
+        }
+    }
+
+    private Block.Locker lock() {
+        String kn = SearchWriter.class.getName()
+                  + ":" + getDbName(  );
+        Locker lk = Block.getLocker(kn);
+        lk.lock();
+        return lk;
+    }
+
+    private static class SearchWriter implements AutoCloseable {
+
+        private IndexWriter writer;
+        private String dbname;
+        private int c = 1;
+
+        public SearchWriter(IndexWriter writer, String dbname) {
+            this.writer = writer;
+            this.dbname = dbname;
+
+            if (0 < Core.DEBUG && 4 != ( 4 & Core.DEBUG )) {
+                CoreLogger.trace("Start the lucene writer for " + dbname);
+            }
+        }
+
+        public IndexWriter conn() {
+            return this.writer;
+        }
+
+        public int  calc() {
+            return  c ;
+        }
+
+        public void open() {
+//          if (c >= 0) {
+                c += 1;
+//          }
+        }
+
+        public void exit() {
+            if (c >= 1) {
+                c -= 1;
+            }
+        }
+
+        @Override
+        public void close() {
+            // 退出时合并索引
+            try {
+                writer.maybeMerge();
+            } catch (IOException x) {
+                CoreLogger.error(x);
+            }
+
+            try {
+                writer.close();
+            } catch (IOException x) {
+                CoreLogger.error(x);
+            } finally {
+                writer = null ;
+            }
+
+            if (0 < Core.DEBUG && 4 != (4 & Core.DEBUG)) {
+                CoreLogger.trace("Close the lucene writer for " + dbname);
+            }
+        }
+
+    }
+
+    /**
+     * 自启一个定时任务,
+     * 每隔一段时间清理,
+     * 如设为  0 不清理,
+     * 默认为 10 分钟
+     */
+    static {
+        long time = Long.parseLong(
+                System.getProperty(Block.class.getName()
+                        + ".cleans.period" , "600000" ));
+        if ( time > 0 ) // 明确设为 0 则不清理
+        new Timer(Block.class.getName()+".cleans", true)
+        .schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Iterator i = Core.GLOBAL_CORE.entrySet().iterator();
+                while  ( i.hasNext() ) {
+                    Map.Entry e = ( Map.Entry ) i.next();
+                    Object    o = e.getValue( );
+                    if (o instanceof SearchWriter) {
+                        SearchWriter w = (SearchWriter)o;
+                        if (w.calc(  ) == 0 ) {
+                            w.close( );
+                            i.remove();
+                        }
+                    }
+                }
+            }
+        }, time, time);
     }
 
 }

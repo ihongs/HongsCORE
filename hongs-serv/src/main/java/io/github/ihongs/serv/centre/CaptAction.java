f08576415ca3220ef9fdae2228bb0fc4d9e0ccce
Hongs <ihongs@live.cn>
2018-08-11 10:17:41 +0800

    验证码增加透明背景规则

--- hongs-serv/src/main/java/io/github/ihongs/serv/centre/CaptAction.java
+++ hongs-serv/src/main/java/io/github/ihongs/serv/centre/CaptAction.java
@@ -22,10 +22,10 @@ public class CaptAction {
         String f  = Synt.declare(helper.getParameter("f"), "");
         String e  = Synt.declare(helper.getParameter("e"), "png" );
 
-        Capts  vc = Capts.captcha(h, b, f, e);
+        Capts  vc = Capts.captcha( h, b, f );
 
         // 设置会话
-        String ss = CoreConfig.getInstance( )
+        String ss = CoreConfig.getInstance()
               .getProperty ("core.capt.session" , "capt");
         helper.setSessibute(ss+"_code", /**/vc.getCode());
         helper.setSessibute(ss+"_time", System.currentTimeMillis());
@@ -36,7 +36,7 @@ public class CaptAction {
         helper.getResponse().setHeader("Pragma", "no-cache");
         helper.getResponse().setHeader("Cache-Control", "no-cache");
 
-        vc.write(e, helper.getResponse( ).getOutputStream());
+        vc.write(e, helper.getResponse().getOutputStream( ));
     }
 
 }

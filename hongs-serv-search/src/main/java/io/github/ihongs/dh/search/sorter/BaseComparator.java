53c69a14d3542949aa64b3b973aaec3324665100
Hongs <ihongs@live.cn>
2018-09-11 18:01:10 +0800

    增加 FindInSet 按指定集合顺序排列

--- hongs-serv-search/src/main/java/io/github/ihongs/dh/search/sorter/BaseComparator.java
+++ hongs-serv-search/src/main/java/io/github/ihongs/dh/search/sorter/BaseComparator.java
@@ -12,15 +12,13 @@ import org.apache.lucene.search.SimpleFieldComparator;
 public abstract class BaseComparator extends SimpleFieldComparator<Long> {
 
     protected final String    name;
-    protected final boolean   desc;
-    protected       long      top;
-    protected       long      bot;
-    protected final long[]    comparedValues;
+    protected       long      top ;
+    protected       long      bot ;
+    protected final long [  ] comparedValues;
     protected BinaryDocValues originalValues;
 
-    public BaseComparator(String name, boolean desc, int hits) {
+    public BaseComparator(String name, int hits) {
         this.name = name;
-        this.desc = desc;
         comparedValues = new long[hits];
     }
 
@@ -40,24 +38,29 @@ public abstract class BaseComparator extends SimpleFieldComparator<Long> {
     }
 
     @Override
-    public Long value(int i) {
+    public Long value (int i ) {
         return comparedValues[i];
     }
 
     @Override
+    public void copy  (int i , int d ) {
+        comparedValues[i] = worth( d );
+    }
+
+    @Override
     public int compare(int i0, int i1) {
-        if (comparedValues[i0] > comparedValues[i1]) {
-            return 1;
-        }
         if (comparedValues[i0] < comparedValues[i1]) {
             return -1;
+        }
+        if (comparedValues[i0] > comparedValues[i1]) {
+            return  1;
         }
         return 0;
     }
 
     @Override
-    public int compareTop(int d) throws IOException {
-        long dist = wxrth(d);
+    public int compareTop   (int d) {
+        long dist = worth(d);
         if (top < dist) {
             return -1;
         }
@@ -68,8 +71,8 @@ public abstract class BaseComparator extends SimpleFieldComparator<Long> {
     }
 
     @Override
-    public int compareBottom(int d) throws IOException {
-        long dist = wxrth(d);
+    public int compareBottom(int d) {
+        long dist = worth(d);
         if (bot < dist) {
             return -1;
         }
@@ -79,27 +82,12 @@ public abstract class BaseComparator extends SimpleFieldComparator<Long> {
         return 0;
     }
 
-    @Override
-    public void copy(int i, int d) throws IOException {
-        comparedValues[i] = wxrth(d);
-    }
-
-    /**
-     * 将值做逆序转换
-     * @param d 文档索引
-     * @return
-     * @throws IOException 
-     */
-    protected /*desc*/ long wxrth(int d) throws IOException {
-        return  desc  ? 0 - worth(d) : worth(d);
-    }
-
     /**
      * 取得并转化数据
      * @param d 文档索引
      * @return
      * @throws IOException
      */
-    protected abstract long worth(int d) throws IOException;
+    protected abstract long worth(int d);
 
 }
--- hongs-serv-search/src/main/java/io/github/ihongs/dh/search/sorter/IntervalSorter.java
+++ hongs-serv-search/src/main/java/io/github/ihongs/dh/search/sorter/IntervalSorter.java
@@ -19,32 +19,44 @@ public class IntervalSorter extends FieldComparatorSource {
     }
 
     @Override
+    public String toString() {
+        return "Interval("+ dist + ")";
+    }
+
+    @Override
     public FieldComparator<?> newComparator(String fn, int nh, int sp, boolean rv) {
-        return new Comparator(fn, rv, nh, dist);
+        return new Comparator(fn, nh, dist);
     }
 
     static public class Comparator extends BaseComparator {
 
         long dist;
 
-        public Comparator(String name, boolean desc, int hits, long dist) {
-            super(name, desc, hits);
-            this.dist = dist;
+        public Comparator(String name, int hits, long dist) {
+            super(name, hits);
+            this.dist = dist ;
         }
 
         @Override
         protected long worth(int d) {
-            BytesRef bytesRef = originalValues.get(d);
-            String   fv = bytesRef.utf8ToString();
-            String[] xy = fv.split(",");
+            BytesRef br = originalValues.get(d);
+            String   fv = br.utf8ToString();
+            if (0 == fv.length()) {
+                return  Long.MAX_VALUE ;
+            }
+
+            String[] xy = fv.split (",", 2);
             long     fx = Long.parseLong(xy[0]);
             long     fy = Long.parseLong(xy[1]);
+
+            // 区间外为正值, 仅计算绝对值
             if (fx > dist) {
                 return fx - dist;
             } else
             if (fy < dist) {
                 return dist - fy;
             }
+
             // 区间内为负值, 比外面都优先
             if (fy - dist > dist - fx) {
                 return fx - dist;

83593554988e71615e7eedad2821aa5d22a248e8
Hongs <Hongs@fe80:0:0:0:142f:3dff:feb8:da10%8>
2018-10-10 21:18:47 +0800

    完善数据操作命令

--- hongs-serv-search/src/main/java/io/github/ihongs/dh/search/SearchCmdlet.java
+++ hongs-serv-search/src/main/java/io/github/ihongs/dh/search/SearchCmdlet.java
@@ -6,7 +6,6 @@ import io.github.ihongs.action.ActionHelper;
 import io.github.ihongs.action.VerifyHelper;
 import io.github.ihongs.cmdlet.CmdletHelper;
 import io.github.ihongs.cmdlet.anno.Cmdlet;
-import io.github.ihongs.dh.lucene.LuceneRecord;
 import io.github.ihongs.util.Synt;
 import java.util.List;
 import java.util.Map;
@@ -34,7 +33,7 @@ public class SearchCmdlet {
 
         String conf = Synt.asString(opts.remove("conf"));
         String name = Synt.asString(opts.remove("name"));
-        LuceneRecord so = LuceneRecord.getInstance(conf, name);
+        SearchEntity so = SearchEntity.getInstance(conf, name);
         Map rsp = so.search(opts);
         CmdletHelper.preview(rsp);
     }
@@ -50,7 +49,7 @@ public class SearchCmdlet {
         String conf = Synt.asString(opts.remove("conf"));
         String name = Synt.asString(opts.remove("name"));
         List<String> ds = Synt.asList(opts.remove("id"));
-        LuceneRecord so = LuceneRecord.getInstance(conf, name);
+        SearchEntity so = SearchEntity.getInstance(conf, name);
 
         try {
             so.begin ( );
@@ -79,7 +78,7 @@ public class SearchCmdlet {
         String conf = Synt.asString(opts.remove("conf"));
         String name = Synt.asString(opts.remove("name"));
         List<String> ds = Synt.asList(opts.remove("id"));
-        LuceneRecord so = LuceneRecord.getInstance(conf, name);
+        SearchEntity so = SearchEntity.getInstance(conf, name);
 
         VerifyHelper vh = new VerifyHelper();
         vh.addRulesByForm(conf, name);
@@ -114,7 +113,7 @@ public class SearchCmdlet {
 
         String conf = Synt.asString(opts.remove("conf"));
         String name = Synt.asString(opts.remove("name"));
-        LuceneRecord so = LuceneRecord.getInstance(conf, name);
+        SearchEntity so = SearchEntity.getInstance(conf, name);
 
         VerifyHelper vh = new VerifyHelper();
         vh.addRulesByForm(conf, name);

df41f0d0ab041aea54b5c1c72951e669e65647a0
Hongs <Hongs@fe80:0:0:0:142f:3dff:feb8:da10%8>
2018-10-10 20:59:23 +0800

    完善数据操作命令

--- hongs-serv-search/src/main/java/io/github/ihongs/dh/search/SearchCmdlet.java
+++ hongs-serv-search/src/main/java/io/github/ihongs/dh/search/SearchCmdlet.java
@@ -1,6 +1,6 @@
 package io.github.ihongs.dh.search;
 
-import io.github.ihongs.Core;
+import io.github.ihongs.Cnst;
 import io.github.ihongs.HongsException;
 import io.github.ihongs.action.ActionHelper;
 import io.github.ihongs.action.VerifyHelper;
@@ -8,7 +8,6 @@ import io.github.ihongs.cmdlet.CmdletHelper;
 import io.github.ihongs.cmdlet.anno.Cmdlet;
 import io.github.ihongs.dh.lucene.LuceneRecord;
 import io.github.ihongs.util.Synt;
-
 import java.util.List;
 import java.util.Map;
 
@@ -23,7 +22,7 @@ public class SearchCmdlet {
     public void search(String[] args) throws HongsException {
         Map opts = CmdletHelper.getOpts(args, new String[ ] {
             "conf=s",
-            "name=s",
+            "form=s",
             "id*s",
             "wd*s",
             "rb*s",
@@ -35,66 +34,130 @@ public class SearchCmdlet {
 
         String conf = Synt.asString(opts.remove("conf"));
         String name = Synt.asString(opts.remove("name"));
-        ActionHelper ah = Core.getInstance(ActionHelper.class);
         LuceneRecord so = LuceneRecord.getInstance(conf, name);
-        Map req = ah.getRequestData();
-        req.putAll(opts);
-        Map rsp = so.search (req);
+        Map rsp = so.search(opts);
         CmdletHelper.preview(rsp);
+    }
+
+    @Cmdlet("delete")
+    public void delete(String[] args) throws HongsException {
+        Map opts = CmdletHelper.getOpts(args, new String[ ] {
+            "conf=s",
+            "form=s",
+            "id*s"
+        });
+
+        String conf = Synt.asString(opts.remove("conf"));
+        String name = Synt.asString(opts.remove("name"));
+        List<String> ds = Synt.asList(opts.remove("id"));
+        LuceneRecord so = LuceneRecord.getInstance(conf, name);
+
+        try {
+            so.begin ( );
+            for (String id  : ds) {
+                so.del( id );
+            }
+            so.commit( );
+        }
+        catch (HongsException ex) {
+            so.revert( );
+            throw ex;
+        }
+        finally {
+            so.close ( );
+        }
     }
 
     @Cmdlet("update")
     public void update(String[] args) throws HongsException {
         Map opts = CmdletHelper.getOpts(args, new String[ ] {
             "conf=s",
-            "name=s",
-            "id*s",
-        });
+            "form=s",
+            "id*s"
+        }); args = (String[]) opts.get("");
 
         String conf = Synt.asString(opts.remove("conf"));
         String name = Synt.asString(opts.remove("name"));
         List<String> ds = Synt.asList(opts.remove("id"));
-        ActionHelper ah = Core.getInstance(ActionHelper.class);
         LuceneRecord so = LuceneRecord.getInstance(conf, name);
-        Map  rd = ah.getRequestData();
 
-        if (!rd.isEmpty()) {
-            // 有数据则校验数据
-            VerifyHelper vh = new VerifyHelper();
-            vh.addRulesByForm(conf, name);
+        VerifyHelper vh = new VerifyHelper();
+        vh.addRulesByForm(conf, name);
+        vh.isPrompt(true);
+        vh.isUpdate(true);
+
+        Map rd = data(args[0]);
             rd = vh.verify(rd);
 
-            try {
-                so.begin ( );
-                for (String id  : ds) {
-                    so.set( id  , rd);
-                }
-                so.commit( );
+        try {
+            so.begin ( );
+            for (String id  : ds) {
+                so.put( id  , rd);
             }
-            catch (HongsException ex) {
-                so.revert( );
-                throw ex;
-            }
-            finally {
-                so.close ( );
-            }
+            so.commit( );
         }
-        else {
-            // 不给内容即为删除
-            try {
-                so.begin( );
-                for (String id  : ds) {
-                    so.del( id );
+        catch (HongsException ex) {
+            so.revert( );
+            throw ex;
+        }
+        finally {
+            so.close ( );
+        }
+    }
+
+    @Cmdlet("create")
+    public void create(String[] args) throws HongsException {
+        Map opts = CmdletHelper.getOpts(args, new String[ ] {
+            "conf=s",
+            "form=s"
+        }); args = (String[]) opts.get("");
+
+        String conf = Synt.asString(opts.remove("conf"));
+        String name = Synt.asString(opts.remove("name"));
+        LuceneRecord so = LuceneRecord.getInstance(conf, name);
+
+        VerifyHelper vh = new VerifyHelper();
+        vh.addRulesByForm(conf, name);
+        vh.isPrompt(true);
+
+        String id;
+        Map    rd;
+
+        try {
+            so.begin ( );
+            for (String rt : args) {
+                rd =    data  (rt);
+                rd = vh.verify(rd);
+                id = (String)  rd.get(Cnst.ID_KEY);
+                if (id != null && id.length() > 0) {
+                    so.set(id, rd);
+                } else {
+                    so.add(    rd);
                 }
-                so.commit( );
             }
-            catch (HongsException ex) {
-                so.revert( );
-                throw ex;
-            }
-            finally {
-                so.close ( );
-            }
+            so.commit( );
+        }
+        catch (HongsException ex) {
+            so.revert( );
+            throw ex;
+        }
+        finally {
+            so.close ( );
+        }
+    }
+
+    private static Map data(String text) {
+        text = text.trim();
+        if (text.startsWith("<") && text.endsWith(">")) {
+            throw  new UnsupportedOperationException("Unsupported html: "+ text);
+        } else
+        if (text.startsWith("[") && text.endsWith("]")) {
+            throw  new UnsupportedOperationException("Unsupported list: "+ text);
+        } else
+        if (text.startsWith("{") && text.endsWith("}")) {
+            return (Map) io.github.ihongs.util.Data.toObject(text);
+        } else {
+            return ActionHelper.parseQuery(text);
         }
     }
 

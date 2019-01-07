2259a297bf7fd9dc4e88d697c14f7a7f3982aec0
Huang Hong <kevin.hong@gmail.com>
2019-01-07 17:54:44 +0800

    对象类型可重复

--- hongs-core/src/main/java/io/github/ihongs/util/verify/NoRepeat.java
+++ hongs-core/src/main/java/io/github/ihongs/util/verify/NoRepeat.java
@@ -1,7 +1,11 @@
 package io.github.ihongs.util.verify;
 
+import io.github.ihongs.HongsException;
+import io.github.ihongs.action.FormSet;
+import io.github.ihongs.util.Synt;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * 单值约束
@@ -10,13 +14,31 @@ import java.util.Map;
 public class NoRepeat extends Rule {
     @Override
     public Object verify(Object value) throws Wrong {
-        if (value instanceof Object[ ] ) {
+        if (value instanceof Map) {
+            /**
+             * 对象类型的字段要放行
+             * 至于下层是否多组取值
+             * 只能在后面另行检验了
+             */
+            try {
+            FormSet fields = FormSet.getInstance ();
+            Object  type   = params.get/**/  ( "__type__"  );
+                Map saves  = fields.getEnum  ( "__saves__" );
+                Set types  = Synt.toSet(saves.get("object"));
+                if (types != null && type != null
+                &&  types.contains ( type )) {
+                    return value;
+                }
+            } catch (HongsException ex) {
+                throw new Wrong(ex.getMessage( ));
+            }
+
             throw new Wrong("fore.form.norepeat");
         }
         if (value instanceof Collection) {
             throw new Wrong("fore.form.norepeat");
         }
-        if (value instanceof Map) {
+        if (value instanceof Object[ ] ) {
             throw new Wrong("fore.form.norepeat");
         }
         return value;

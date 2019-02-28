3620ffa88c1c7bd10ded6bb3e4591e721ddda316
Hongs <ihongs@live.cn>
2019-02-28 22:47:40 +0800

    取消注册时分配前端权限, 仅分配前端组即可

--- hongs-serv-master/src/main/java/io/github/ihongs/serv/centre/SignAction.java
+++ hongs-serv-master/src/main/java/io/github/ihongs/serv/centre/SignAction.java
@@ -64,17 +64,17 @@ public class SignAction extends io.github.ihongs.serv.centra.SignAction {
         String appid = Synt.declare(ah.getParameter("appid"), "_WEB_");
         String place = Synt.declare(ah.getParameter("place"),"centre");
 
-        // 赋予公共权限
-        sd = new HashMap();
-        sd.put("user_id", usrid);
-        sd.put("role"   , cc.getProperty("core.public.regs.role", "centre"));
-        uo.db.getTable("user_role").insert(sd);
-
         // 加入公共部门
         sd = new HashMap();
         sd.put("user_id", usrid);
         sd.put("dept_id", cc.getProperty("core.public.regs.dept", "CENTRE"));
         uo.db.getTable("user_dept").insert(sd);
+
+        // 赋予公共权限. 仅用部门限权(2019/02/28)
+//      sd = new HashMap();
+//      sd.put("user_id", usrid);
+//      sd.put("role"   , cc.getProperty("core.public.regs.role", "centre"));
+//      uo.db.getTable("user_role").insert(sd);
 
         ah.reply(AuthKit.userSign(ah, place, appid, usrid, uname, uhead, utime));
     }

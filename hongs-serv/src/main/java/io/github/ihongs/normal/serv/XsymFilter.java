21fe682ace97a8fc6dbaef647b2d08ae1ea92a22
Hongs <Hongs@fe80:0:0:0:142f:3dff:feb8:da10%8>
2019-07-28 15:47:37 +0800

    去除旧版关系符的冒号前缀, 以便兼容旧的请求格式

---
+++ hongs-serv/src/main/java/io/github/ihongs/normal/serv/XsymFilter.java
@@ -0,0 +1,151 @@
+package io.github.ihongs.normal.serv;
+
+import io.github.ihongs.Core;
+import io.github.ihongs.action.ActionDriver;
+import io.github.ihongs.action.ActionHelper;
+import io.github.ihongs.action.PasserHelper;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 去除关系符的冒号前缀
+ * @deprecated 仅为兼容
+ * @author Hongs
+ */
+public class XsymFilter extends ActionDriver {
+
+    /**
+     * 不包含的URL
+     */
+    private PasserHelper ignore = null;
+
+    @Override
+    public void init(FilterConfig config)
+        throws ServletException
+    {
+        super.init(config);
+
+        /**
+         * 获取不包含的URL
+         */
+        this.ignore = new PasserHelper(
+            config.getInitParameter("ignore-urls"),
+            config.getInitParameter("attend-urls")
+        );
+    }
+
+    @Override
+    public void destroy()
+    {
+        super.destroy();
+
+        ignore = null;
+    }
+
+    @Override
+    public void doFilter(Core core, ActionHelper hlpr, FilterChain chain)
+        throws IOException, ServletException
+    {
+        HttpServletResponse rsp = hlpr.getResponse();
+        HttpServletRequest  req = hlpr.getRequest( );
+        String act = ActionDriver.getRecentPath(req);
+        String ctt = req.getContentType();
+
+        /**
+         * 非 JSON 类型已在 Dict 中作兼容
+         */
+        if (ctt == null) {
+            chain.doFilter(req, rsp);
+            return;
+        }
+        ctt = ctt.split( ";", 2 )[0];
+        if (! ctt.endsWith("/json")) {
+            chain.doFilter(req, rsp);
+            return;
+        }
+
+        /**
+         * 检查当前动作是否可以忽略
+         */
+        if (ignore != null && ignore.ignore( act ) ) {
+            chain.doFilter(req, rsp);
+            return;
+        }
+
+        doChange(hlpr.getRequestData());
+    }
+
+    private void doChange(Map rd) {
+        Iterator<Map.Entry> it = rd.entrySet().iterator();
+        Map rp = new HashMap();
+
+        while ( it.hasNext() ) {
+            Map.Entry  et = it.next();
+            Object v = et.getValue( );
+            Object k = et.getKey  ( );
+
+            if (v instanceof Map) {
+                doChange((Map) v);
+            } else
+            if (v instanceof Collection) {
+                doChange((Collection) v);
+            } else
+            if (v instanceof Object [ ]) {
+                doChange((Object [ ]) v);
+            }
+
+            // 去掉键的冒号前缀
+            if (k instanceof String) {
+                String  n = (String) k;
+                if (n.length( )  ==  3
+                &&  n.startsWith(":")) {
+                    n = n.substring(1);
+                    rp.put(n,v);
+                    it.remove();
+                }
+            }
+        }
+
+        // 写入清理后的数据
+        if (! rp.isEmpty()) {
+            rd.putAll( rp );
+        }
+    }
+
+    private void doChange(Collection rc) {
+        for (Object v : rc) {
+            if (v instanceof Map) {
+                doChange((Map) v);
+            } else
+            if (v instanceof Collection) {
+                doChange((Collection) v);
+            } else
+            if (v instanceof Object [ ]) {
+                doChange((Object [ ]) v);
+            }
+        }
+    }
+
+    private void doChange(Object [ ] ra) {
+        for (Object v : ra) {
+            if (v instanceof Map) {
+                doChange((Map) v);
+            } else
+            if (v instanceof Collection) {
+                doChange((Collection) v);
+            } else
+            if (v instanceof Object [ ]) {
+                doChange((Object [ ]) v);
+            }
+        }
+    }
+
+}

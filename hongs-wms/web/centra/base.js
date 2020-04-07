a6b33265d5ada2a597a9bc1b9b2181b8cb11c15a
Hongs <ihongs@live.cn>
2020-04-07 19:15:40 +0800

    累积优化

--- hongs-wms/web/centra/base.js
+++ hongs-wms/web/centra/base.js
@@ -344,8 +344,8 @@ function hsCopyListData(box) {
     var tr  = $('<tr></tr>');
     var th  = $('<th></th>');
     var td  = $('<td></td>');
-    th .attr("style", 'white-space: pre-line; mso-number-format: "\\@";');
-    td .attr("style", 'white-space: pre-line; mso-number-format: "\\@";');
+//  th .attr("style", 'white-space: pre-line; mso-number-format: "\\@";');
+//  td .attr("style", 'white-space: pre-line; mso-number-format: "\\@";');
     div.attr("style", "height: 1px; display: none; overflow: auto;");
     tab.attr("style", "margin: 1px;");
     tab.attr("class", "table table-bordered table-copylist");

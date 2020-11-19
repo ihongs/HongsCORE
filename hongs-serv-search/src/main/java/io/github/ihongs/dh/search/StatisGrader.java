be6b2ede968450d04b2523b8f1772b5737ec7c51
Kevin <Kevin@DESKTOP-N5M2QPU.lan>
2020-11-20 00:00:51 +0800

    新的分维度指标计算工具

--- hongs-serv-search/src/main/java/io/github/ihongs/dh/search/StatisGrader.java
+++ hongs-serv-search/src/main/java/io/github/ihongs/dh/search/StatisGrader.java
@@ -1,9 +1,35 @@
 package io.github.ihongs.dh.search;
 
+import java.io.IOException;
+import java.util.Map;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+
 /**
  * 分类统计工具
  * @author Kevin
  */
 public class StatisGrader {
-    
+
+    public static enum TYPE {
+        INT , LONG , FLOAT , DOUBLE , STRING , // 单数
+        INTS, LONGS, FLOATS, DOUBLES, STRINGS  // 复数
+    };
+
+    private final IndexSearcher finder;
+    private       Query   query ;
+
+    public StatisGrader (IndexSearcher finder) {
+        this.finder = finder;
+    }
+
+    public StatisGrader where(Query query) {
+        this.query = query;
+        return this;
+    }
+
+    public Map<String, Map<Object, Object[]>> fetch() throws IOException {
+        return null;
+    }
+
 }

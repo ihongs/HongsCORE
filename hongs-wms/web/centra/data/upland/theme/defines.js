170696d1608607035c474c08652af144e2c7e3d7
Hongs <ihongs@live.cn>
2020-11-26 22:27:40 +0800

    去掉批量操作

--- hongs-wms/web/centra/data/upland/theme/defines.js
+++ hongs-wms/web/centra/data/upland/theme/defines.js
@@ -1,7 +1,17 @@
 
 function in_centra_data_upland_theme_list(context, listobj) {
+    // 去掉批量操作
+    context.find(".toolbox .for-choose").remove();
+    context.find(".toolbox .for-checks").remove();
+    context.find(".listbox [data-ft=_check]")
+           .attr("data-ft", "_rowid")
+           .attr("data-fn", /**/"id")
+           .attr( "class" , "_rowid");
+
     listobj._fill_name = function(td, v) {
-        td.append($('<a href="javascript:;" class="review"></a>').text(v));
+        $('<a href="javascript:;" class="review"></a>')
+            .appendTo(td)
+            .text(v);
     }
 }
 

281f0602c8edee86ab559b422109ca263ad2bfc0
Kevin <Kevin@DESKTOP-N5M2QPU.lan>
2020-11-26 22:27:40 +0800

    去掉批量操作

---
+++ hongs-wms/web/centra/data/upland/tweet/defines.js
@@ -0,0 +1,10 @@
+
+function in_centra_data_upland_tweet_list(context, listobj) {
+    // 去掉批量操作
+    context.find(".toolbox .for-choose").remove();
+    context.find(".toolbox .for-checks").remove();
+    context.find(".listbox [data-ft=_check]")
+           .attr("data-ft", "_rowid")
+           .attr("data-fn", /**/"id")
+           .attr( "class" , "_rowid");
+}

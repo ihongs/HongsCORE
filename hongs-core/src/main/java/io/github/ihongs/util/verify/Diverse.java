41301ae559734ced4d00123e196b928297b17fc5
Hongs <ihongs@live.cn>
2020-12-17 21:53:29 +0800

    新的 Unique Key 逻辑

--- hongs-core/src/main/java/io/github/ihongs/util/verify/Diverse.java
+++ hongs-core/src/main/java/io/github/ihongs/util/verify/Diverse.java
@@ -1,12 +1,13 @@
 package io.github.ihongs.util.verify;
 
 import io.github.ihongs.Cnst;
-import io.github.ihongs.Core;
 import io.github.ihongs.HongsException;
 import io.github.ihongs.action.ActionHelper;
 import io.github.ihongs.action.ActionRunner;
 import io.github.ihongs.util.Synt;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -16,25 +17,50 @@ import java.util.Set;
  *
  * <pre>
  * 规则参数:
- *  data-ut 查询动作名, 末尾可添加 #其他参与唯一的字段
+ *  data-ut 查询动作名
+ *  data-uk 唯一字段名, 缺省为当前字段
  *  diverse 并非用在这, 而是被用于 Repeated 中去除重复
+ * 注意事项:
+ *  对于可以存储多个值的字段请慎重使用
  * </pre>
  *
  * @author Hongs
  */
 public class Diverse extends Rule {
 
+    private  final  Object FORCE;
+
+    /**
+     * 全局唯一校验
+     * @param force
+     */
+    public Diverse(boolean force) {
+        if (force) {
+            FORCE = BLANK;
+        } else {
+            FORCE = STAND;
+        }
+    }
+
+    public Diverse() {
+        this (false);
+    }
+
     @Override
     public Object verify(Value watch) throws Wrong {
         // 跳过空值和空串
-        Object value = watch.get();
-        if (value  ==  null ) {
-            return STAND;
-        }
-        if (value.equals("")) {
-            return STAND;
+        Object  value  = watch.get( );
+        if (FORCE == STAND) {
+            if (value  ==  null ) {
+                return STAND;
+            }
+            if (value.equals("")) {
+                return STAND;
+            }
         }
 
+        Map nd = watch.getCleans(     );
+        Object id = nd.get(Cnst.ID_KEY);
         String ut = (String) getParam("data-ut" );
         String uk = (String) getParam("data-uk" );
         String nk = (String) getParam("__name__");
@@ -42,9 +68,9 @@ public class Diverse extends Rule {
         String fk = (String) getParam("__form__");
 
         if (ut == null || ut.isEmpty()) {
-            ut = ck + "/" + fk + "/search" ;
+            ut = ck +"/"+ fk +"/search";
         }
-        if (uk == null && uk.isEmpty()) {
+        if (uk == null || uk.isEmpty()) {
             uk = nk ;
         }
 
@@ -53,20 +79,94 @@ public class Diverse extends Rule {
         Map rd = new HashMap();
         rd.put(Cnst.PN_KEY, 0);
         rd.put(Cnst.RN_KEY, 1);
-        rd.put(Cnst.RB_KEY, Synt.setOf (  Cnst.ID_KEY  ) );
+        rd.put(Cnst.RB_KEY, Synt.setOf(Cnst.ID_KEY));
 
         // 更新需排除当前记录
         if (watch.isUpdate( )) {
-            Object vo = watch.getValues().get(Cnst.ID_KEY);
             Map ne = new HashMap( );
-            ne.put(Cnst.NE_REL, vo);
+            ne.put(Cnst.NE_REL, id);
             rd.put(Cnst.ID_KEY, ne);
         }
 
         // 参与唯一约束的字段
-        Set<String> ks = Synt.toTerms ( uk );
-        if (null != ks) for (String kn: ks ) {
-            rd.put( kn , watch.getValues().get(kn) );
+        Set us = Synt.toTerms(uk);
+        Set ns = new HashSet (us);
+        Iterator<String> it = ns.iterator( );
+        while  (it.hasNext( )) {
+            String n = it.next( );
+            Object v ;
+            if (nd.containsKey(n)) {
+                it.remove ( );
+                v = nd.get(n);
+            } else
+            if (nk.equals (n)) {
+                it.remove ( );
+                v = value;
+            } else {
+                continue ;
+            }
+
+            if (v == null) {
+                rd.put(n, Synt.mapOf(Cnst.IS_REL, "null"));
+            } else
+            if (v.equals("") ) {
+                rd.put(n, Synt.mapOf(Cnst.EQ_REL,   v   ));
+            } else {
+                rd.put(n, v);
+            }
+        }
+
+        // 没提供任何值则跳过
+        if (ns.size() == us.size()) {
+            return FORCE;
+        }
+
+        // 补充缺的旧的字段值
+        if (watch.isUpdate() && !ns.isEmpty()) {
+            Map ud = new HashMap( );
+            ud.put(Cnst.ID_KEY, id);
+            ud.put(Cnst.RB_KEY, ns);
+            ud.put(Cnst.RN_KEY, 0 );
+
+            ActionHelper ah = ActionHelper.newInstance();
+            ah.setContextData( cd );
+            ah.setRequestData( ud );
+            try {
+                ActionRunner.newInstance(ah, ut).doInvoke();
+            } catch (HongsException ex) {
+                throw ex.toExemption( );
+            }
+
+            SD: {
+                Map sd  = ah.getResponseData(  );
+                if (sd == null) {
+                    break SD;
+                }
+
+                if (sd.containsKey("list")) {
+                    List sl = (List) sd.get("list");
+                    if ( sl . isEmpty( ) ) break SD;
+                    sd = (Map) sl.get(  00  );
+                } else
+                if (sd.containsKey("info")) {
+                    sd = (Map) sd.get("info");
+                } else {
+                    break SD;
+                }
+
+                for(Object n : ns) {
+                    Object v = sd.get(n);
+                    if (v == null) {
+                        rd.put(n, Synt.mapOf(Cnst.IS_REL, "null"));
+                    } else
+                    if (v.equals("")) {
+                        rd.put(n, Synt.mapOf(Cnst.EQ_REL,   v   ));
+                    } else
+                    {
+                        rd.put(n, v);
+                    }
+                }
+            }
         }
 
         // 执行动作
@@ -82,32 +182,32 @@ public class Diverse extends Rule {
         // 对比结果
         Map sd  = ah.getResponseData();
         if (sd == null) {
-                return value;
+                return FORCE;
         }
         if (sd.containsKey("list")) {
            List list = (List) sd.get("list");
             if (list == null || list.isEmpty()) {
-                return value;
+                return FORCE;
             }
         } else
         if (sd.containsKey("info")) {
             Map info = (Map ) sd.get("info");
             if (info == null || info.isEmpty()) {
-                return value;
+                return FORCE;
             }
         } else
         if (sd.containsKey("page")) {
             Map page = (Map ) sd.get("page");
             if (page == null || page.isEmpty()) {
-                return value;
+                return FORCE;
             } else
             if (page.containsKey("count")
             &&  Synt.declare(page.get("count"), 0) == 0) {
-                return value;
+                return FORCE;
             } else
             if (page.containsKey("pages")
             &&  Synt.declare(page.get("pages"), 0) == 0) {
-                return value;
+                return FORCE;
             }
         }
 

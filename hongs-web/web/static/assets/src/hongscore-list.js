6f4a644069ecc4ca795f354c9d54ce75dd4ba438
Hongs <ihongs@live.cn>
2021-04-02 19:04:24 +0800

    完善错误消息资源

--- hongs-web/web/static/assets/src/hongscore-list.js
+++ hongs-web/web/static/assets/src/hongscore-list.js
@@ -465,7 +465,7 @@ HsList.prototype = {
             if (rst.msg) {
                 this.warn(rst.msg, "warning");
             } else {
-                this.warn(hsGetLang('error.unkwn'));
+                this.warn(hsGetLang('error.undef'));
             }
             return;
         }

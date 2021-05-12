1f715ed85f9e854e88ad3d63593173ada90164e5
Kevin <Kevin@DESKTOP-N5M2QPU.lan>
2021-05-12 23:28:27 +0800

    增加级联操作和冗余同步

--- hongs-serv-matrix/src/main/java/io/github/ihongs/serv/matrix/Casc.java
+++ hongs-serv-matrix/src/main/java/io/github/ihongs/serv/matrix/Casc.java
@@ -1,9 +1,12 @@
 package io.github.ihongs.serv.matrix;
 
+import io.github.ihongs.Cnst;
 import io.github.ihongs.Core;
 import io.github.ihongs.CoreLogger;
 import io.github.ihongs.HongsException;
+import io.github.ihongs.util.Synt;
 import io.github.ihongs.util.reflex.Async;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -12,63 +15,108 @@ import java.util.Set;
  */
 public class Casc {
 
-    private static Queue  QUEUE = new Queue ( );
-    private static enum   PROC {UPDATE, DELETE};
-    public  final  PROC   proc ;
-    public  final  String conf ;
-    public  final  String form ;
-    public  final  String fk   ;
-    public  final  Set<String> fv;
-
-    private  Casc ( PROC proc, String conf, String form, String fk, Set<String> fv) {
-        this.proc = proc ;
-        this.conf = conf ;
-        this.form = form ;
-        this.fk   = fk   ;
-        this.fv   = fv   ;
-    }
-    
-    public static void update (String conf, String form, String fk, Set<String> fv) {
-        QUEUE.add(new Casc(PROC.UPDATE,  conf, form, fk, fv));
-    }
-    
-    public static void delete (String conf, String form, String fk, Set<String> fv) {
-        QUEUE.add(new Casc(PROC.DELETE,  conf, form, fk, fv));
-    }
-    
-    private static class Queue extends Async<Casc> {
-
-        public Queue () {
-            super("matrix.cascade", Integer.MAX_VALUE, 1);
-        }
-        
+    private static Async QUEUE = new Async<Casc> (
+        "matrix.cascade" , Integer.MAX_VALUE , 1
+    ) {
         @Override
-        public void run(Casc data) {
-            Core core = Core.getInstance();
-            long time = System.currentTimeMillis() / 1000;
-            try {
-                Data inst = Data.getInstance(data.conf, data.form);
-                switch (data.proc) {
-                    case UPDATE:
-                        for (String fv : data.fv ) {
-                            inst.updateCascade(data.fk, fv , time);
-                        }
-                        break;
-                    case DELETE:
-                        for (String fv : data.fv ) {
-                            inst.deleteCascade(data.fk, fv , time);
-                        }
-                        break;
-                }
-            }
-            catch (HongsException ex) {
-                CoreLogger.error (ex);
-            }
-            finally {
-                core.close( );
-            }
-        }
-        
+        public void run(Casc casc) { casc.run(); }
+    };
+
+    public  final  String conf;
+    public  final  String form;
+    public  final  Set<String> setIds;
+    public  final  Set<String> delIds;
+
+    private Casc (String conf, String form, Set<String> setIds, Set<String> delIds) {
+        this.conf = conf;
+        this.form = form;
+        this.setIds = setIds;
+        this.delIds = delIds;
     }
     
+    private void run() {
+        Core core = Core.getInstance();
+        try {
+            cascade(Data.getInstance(conf, form), setIds, delIds);
+        }
+        catch (Exception|Error e) {
+            CoreLogger.error ( e);
+        }
+        finally {
+            core.close( );
+        }
+    }
+    
+    public static void add(String conf, String form, Set<String> setIds, Set<String> delIds) {
+        QUEUE.add(new Casc( conf, form, setIds, delIds ) );
+    }
+    
+    public static void cascade(Data inst, Set<String> setIds, Set<String> delIds) throws HongsException {
+        Set<String> ats = Synt.toSet(inst.getParams().get("cascades"));
+        if (ats == null || ats.isEmpty()) {
+            return;
+        }
+        if (setIds.isEmpty( )
+        &&  delIds.isEmpty()) {
+            return;
+        }
+        long ct = System.currentTimeMillis() / 1000;
+        for(String at : ats) {
+            // 格式: conf.form?fk#DELETE#UPDATE
+            int p ;
+            p = at.indexOf("#");
+            String tk = at.substring(0+p);
+                   at = at.substring(0,p);
+            p = at.indexOf("?");
+            String fk = at.substring(1+p);
+                   at = at.substring(0,p);
+            p = at.lastIndexOf(".");
+            String c  = at.substring(0,p);
+            String f  = at.substring(1+p);
+
+            // 获取实例, 逐一处理
+            Data   md = Data.getInstance( c , f );
+            if (!delIds.isEmpty()
+            &&  tk.contains( "#DELETE" )) {
+                deleteCascade(md, fk, delIds, ct);
+            }
+            if (!setIds.isEmpty()
+            &&  tk.contains( "#UPDATE" )) {
+                updateCascade(md, fk, setIds, ct);
+            }
+        }
+    }
+
+    public static void updateCascade(Data inst, String fk, Set<String> fv, long ct) throws HongsException {
+        Data.Loop loop = inst.search(Synt.mapOf(
+            Cnst.RB_KEY, Synt.setOf(Cnst.ID_KEY),
+            fk, fv
+        ), 0, 0);
+        String  fn = inst.getFormId();
+        for (Map info : loop) {
+            String id = (String) info.get(Cnst.ID_KEY);
+            inst.set(id, Synt.mapOf(
+                "__meno__" , "system.cascade",
+                "__memo__" , "Update cascade " + fn + ":" + id
+            ) , ct);
+            CoreLogger.debug("Update cascade {} {}:{}", fn, fk, id);
+        }
+    }
+
+    public static void deleteCascade(Data inst, String fk, Set<String> fv, long ct) throws HongsException {
+        Data.Loop loop = inst.search(Synt.mapOf(
+            Cnst.RB_KEY, Synt.setOf(Cnst.ID_KEY),
+            fk, fv
+        ), 0, 0);
+        String  fn = inst.getFormId();
+        for (Map info : loop) {
+            String id = (String) info.get(Cnst.ID_KEY);
+            inst.cut(id, Synt.mapOf(
+                "__meno__" , "system.cascade",
+                "__memo__" , "Delete cascade " + fn + ":" + id
+            ) , ct);
+            CoreLogger.debug("Delete cascade {} {}:{}", fn, fk, id);
+        }
+    }
+
 }

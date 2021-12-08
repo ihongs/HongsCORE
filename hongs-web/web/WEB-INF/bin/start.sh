62fe257cb1ae7b4beb315fd59f21afabd9025da9
Hongs <ihongs@live.cn>
2021-12-08 22:17:12 +0800

    新的启动脚本

--- hongs-web/web/WEB-INF/bin/start.sh
+++ hongs-web/web/WEB-INF/bin/start.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-# Just for docker starts.
-
 DEBUG=2
 SPORT=8080
-PWD=$(cd `dirname $0`; pwd)
-CWD=`dirname $PWD`
-APP="$CWD/bin/hdo"
+CWD="$(dirname "$0")"
+CWD="$(cd "$PWD"/..; pwd)"
 PID="$CWD/var/server/$SPORT.pid"
+LOG="$CWD/var/log/run.out"
+APP="$CWD/bin/hdo"
+USR="nginx"
 
 # Make sure the process is stoped, and pid file is removed
 if [ -f "$PID" ]
 then
-  kill `cat "$PID"`
-  rm -f "$PID"
-  sleep 3
+  PID=`cat "$PID"`
+  kill $PID
+  # Wati 3 seconds
+  if ps -p $PID > /dev/null; then sleep 1
+  if ps -p $PID > /dev/null; then sleep 1
+  if ps -p $PID > /dev/null; then sleep 1
+  if ps -p $PID > /dev/null; then echo 'Start timeout'; exit
+  fi; fi; fi; fi
 fi
 
+# start for docker
 exec "$APP" server.start $SPORT --DEBUG $DEBUG
+
+# run in background
+#nohup sh "$APP" server.start $SPORT --DEBUG $DEBUG > "$LOG" 2>&1 &
+
+# run in background with user, must export JAVA_HOME
+#su -m -s /bin/bash -c "nohup sh '$APP' server.start $SPORT --DEBUG $DEBUG > '$LOG' 2>&1 &" $USR

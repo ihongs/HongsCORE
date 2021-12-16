deae07a0ec7115d6c0cb9ac9c893c70e1f4a3105
Hongs <ihongs@live.cn>
2021-12-16 22:48:43 +0800

    外部请求参数限定过滤

--- hongs-serv/src/main/java/io/github/ihongs/normal/serv/WaryFilter.java
+++ hongs-serv/src/main/java/io/github/ihongs/normal/serv/WaryFilter.java
@@ -28,8 +28,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * 设置参数:
  * rn-limit 列表查询数量限定, 如 100
- * xr-limit 复合条件数量限定, 如 1
- * xr-level 复合条件层级限定, 如 1
+ * sr-limit 复合条件数量限定, 如 2
+ * sr-level 复合条件层数限定, 如 1
  * illegals 非法参数, 如 or,nr,ar
  *
  * @author Hongs
@@ -38,8 +38,8 @@ public class WaryFilter extends ActionDriver {
 
     private URLPatterns patter = null;
     private Set illegals = null;
-    private int xr_level = 0;
-    private int xr_limit = 0;
+    private int sr_level = 0;
+    private int sr_limit = 0;
     private int rn_limit = 0;
 
     @Override
@@ -53,10 +53,10 @@ public class WaryFilter extends ActionDriver {
             config.getInitParameter("url-exclude")
         );
 
-        illegals = Synt.toSet(config.getInitParameter("illegals"));
-        xr_level = Synt.asInt(config.getInitParameter("xr-level"));
-        xr_limit = Synt.asInt(config.getInitParameter("xr-limit"));
-        rn_limit = Synt.asInt(config.getInitParameter("rn-limit"));
+        illegals = Synt.toSet  (config.getInitParameter("illegals")   );
+        sr_level = Synt.declare(config.getInitParameter("sr-level"), 0);
+        sr_limit = Synt.declare(config.getInitParameter("sr-limit"), 0);
+        rn_limit = Synt.declare(config.getInitParameter("rn-limit"), 0);
     }
 
     @Override
@@ -66,8 +66,8 @@ public class WaryFilter extends ActionDriver {
 
         patter   = null;
         illegals = null;
-        xr_level = 2;
-        xr_limit = 0;
+        sr_level = 0;
+        sr_limit = 0;
         rn_limit = 0;
     }
 
@@ -120,10 +120,10 @@ public class WaryFilter extends ActionDriver {
             }
         }
 
-        if (xr_limit != 0
-        ||  xr_level != 0) {
+        if (sr_limit != 0
+        ||  sr_level != 0) {
             try {
-                xrCheck(rd, xr_limit, xr_level, 0,1);
+                srCheck(rd, sr_limit, sr_level, 0,1);
             }
             catch (HongsException|HongsExemption ex) {
                 hlpr.fault( ex );
@@ -133,47 +133,47 @@ public class WaryFilter extends ActionDriver {
         chain.doFilter(req, rsp);
     }
 
-    private int xrCheck(Object od, int limit, int level, int t, int l) throws HongsException {
+    private int srCheck(Object od, int limit, int level, int t, int l) throws HongsException {
         if (od == null) {
             return t;
         }
         if (od instanceof Map) {
             Map rd = (Map) od ;
-            xrLevel (level, l);
-            t = xrCount(rd.get(Cnst.AR_KEY), limit, level, t, l);
-            t = xrCount(rd.get(Cnst.NR_KEY), limit, level, t, l);
-            t = xrCount(rd.get(Cnst.OR_KEY), limit, level, t, l);
+            srLevel (level, l);
+            t = srCount(rd.get(Cnst.AR_KEY), limit, level, t, l);
+            t = srCount(rd.get(Cnst.NR_KEY), limit, level, t, l);
+            t = srCount(rd.get(Cnst.OR_KEY), limit, level, t, l);
             return t;
         } else {
             throw new HongsException(400, Cnst.AR_KEY+"/"+Cnst.NR_KEY+"/"+Cnst.OR_KEY+" item must be json object");
         }
     }
 
-    private int xrCount(Object od, int limit, int level, int t, int l) throws HongsException {
+    private int srCount(Object od, int limit, int level, int t, int l) throws HongsException {
         if (od == null) {
             return t;
         }
         if (od instanceof Map) {
             t = t + ((Map) od).size( );
-            xrLimit (limit, t);
+            srLimit (limit, t);
             for(Object xd : ((Map) od).values()) {
-                t = xrCheck((Map) xd, limit, level, t, l + 1);
+                t = srCheck((Map) xd, limit, level, t, l + 1);
             }
             return t;
         } else
         if (od instanceof Collection) {
             t = t + ((Collection) od).size( );
-            xrLimit (limit, t);
+            srLimit (limit, t);
             for(Object xd : ((Collection) od)) {
-                t = xrCheck((Map) xd, limit, level, t, l + 1);
+                t = srCheck((Map) xd, limit, level, t, l + 1);
             }
             return t;
         } else
         if (od instanceof Object [] ) {
             t = t + ((Object [] ) od).length ;
-            xrLimit (limit, t);
+            srLimit (limit, t);
             for(Object xd : ((Object [] ) od)) {
-                t = xrCheck((Map) xd, limit, level, t, l + 1);
+                t = srCheck((Map) xd, limit, level, t, l + 1);
             }
             return t;
         } else {
@@ -181,13 +181,13 @@ public class WaryFilter extends ActionDriver {
         }
     }
 
-    private void xrLimit(int limit, int t) throws HongsException {
+    private void srLimit(int limit, int t) throws HongsException {
         if (limit != 0 && limit < t) {
             throw new HongsException(400, Cnst.AR_KEY+"/"+Cnst.NR_KEY+"/"+Cnst.OR_KEY+" can not exceed "+limit+" items" );
         }
     }
 
-    private void xrLevel(int level, int l) throws HongsException {
+    private void srLevel(int level, int l) throws HongsException {
         if (level != 0 && level < l) {
             throw new HongsException(400, Cnst.AR_KEY+"/"+Cnst.NR_KEY+"/"+Cnst.OR_KEY+" can not exceed "+level+" layers");
         }

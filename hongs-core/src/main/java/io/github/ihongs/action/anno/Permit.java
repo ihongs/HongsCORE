cc70c94dee0ccae69be935d7b466cc38724861a2
Hongs <ihongs@live.cn>
2022-05-25 21:00:44 +0800

    完善权限注解过滤和注释

--- hongs-core/src/main/java/io/github/ihongs/action/anno/Permit.java
+++ hongs-core/src/main/java/io/github/ihongs/action/anno/Permit.java
@@ -6,7 +6,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 权限校验
+ * 权限控制
+ *
+ * 默认判断当前动作的权限, 注意: 既无斜杆前缀, 也无扩展后缀
+ * role 的取值
+ *  @开头表示检查对应动作, 如 @xxx/yyy.zzz 检查是否拥有 xxx/yyy.zzz 的动作权限
+ *  $开头表示检查对应角色, 如 @xxx/yyy/zzz 检查是否拥有 xxx/yyy/zzz 的角色权限
+ *  其他检查对应角色, 与上不同, 用户没有对应角色即无权, 上面当未配置时认为有权
+ *
  * @author Hong
  */
 @Target(ElementType.METHOD)

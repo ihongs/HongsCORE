5da7ec734a7213f974e69b4cbbafd099a212af2d
Hongs <ihongs@live.cn>
2022-06-06 22:37:41 +0800

    针对异常无论何种状态总是显示完整错误栈

--- hongs-core/src/main/java/io/github/ihongs/CoreLogger.java
+++ hongs-core/src/main/java/io/github/ihongs/CoreLogger.java
@@ -223,14 +223,9 @@ public final class CoreLogger
      */
     public static void error(Throwable flaw) {
         String text ;
-        if (4 == (4 & Core.DEBUG)) {
-            // 调试模式下取完整错误栈
-            ByteArrayOutputStream buff = new ByteArrayOutputStream();
-            flaw.printStackTrace( new PrintStream( buff ) ) ;
-            text = buff.toString().replaceAll("(\r\n|\r|\n)","$1\t");
-        } else {
-            text = flaw.toString().replaceAll("(\r\n|\r|\n)","$1\t");
-        }
+        ByteArrayOutputStream buff = new ByteArrayOutputStream();
+        flaw.printStackTrace( new PrintStream( buff ) ) ;
+        text = buff.toString().replaceAll("(\r\n|\r|\n)","$1\t");
 
         if (1 == (1 & Core.DEBUG)) {
             getLogger(Cnst.LOG_NAME).error(mark(text));

28bb50db61c79353b4be7abc75bb252779b5e725
Hongs <ihongs@live.cn>
2022-06-27 20:11:05 +0800

    完善异常处理

--- hongs-core/src/main/java/io/github/ihongs/action/ActionDriver.java
+++ hongs-core/src/main/java/io/github/ihongs/action/ActionDriver.java
@@ -320,32 +320,34 @@ public class ActionDriver implements Filter, Servlet {
                 doCommit(core, hlpr, req, rsq );
             } catch (IOException ex) {
                 // 非调试模式忽略客户端中途断开
-                Throwable cx = ex.getCause( );
-                if (cx != null
-                &&  4  != (4 & Core.DEBUG)
-                &&  cx.getClass().getSimpleName().equalsIgnoreCase("EofException")) {
-                    return;
-                }
-                if (4  != (4 & Core.DEBUG)
-                &&  ex.getClass().getSimpleName().equalsIgnoreCase("EofException")) {
-                    return;
+                if (4 != (4 & Core.DEBUG)) {
+                    Throwable cx = ex.getCause( );
+                    if (cx != null
+                    &&  cx.getClass().getSimpleName().equalsIgnoreCase("EofException")) {
+                        throw ex;
+                    }
+                    if (ex.getClass().getSimpleName().equalsIgnoreCase("EofException")) {
+                        throw ex;
+                    }
                 }
 
                 CoreLogger.error(ex);
                 throw ex;
             } catch (ServletException | RuntimeException ex) {
                 // 30x,40x 错误不需要记录到日志
-                Throwable cx = ex.getCause( );
-                if (cx instanceof HongsCause) {
-                    int   st = ( (HongsCause) cx).getState();
-                    if (300 <= st  &&  st <= 499) {
-                        throw  ex;
+                if (4 != (4 & Core.DEBUG)) {
+                    Throwable cx = ex.getCause( );
+                    if (cx instanceof HongsCause) {
+                        int st = ((HongsCause)cx).getState();
+                        if (299 < st && st < 500) {
+                            throw ex;
+                        }
                     }
-                }
-                if (ex instanceof HongsCause) {
-                    int   st = ( (HongsCause) ex).getState();
-                    if (300 <= st  &&  st <= 499) {
-                        throw  ex;
+                    if (ex instanceof HongsCause) {
+                        int st = ((HongsCause)ex).getState();
+                        if (299 < st && st < 500) {
+                            throw ex;
+                        }
                     }
                 }
 

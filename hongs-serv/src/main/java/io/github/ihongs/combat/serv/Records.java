1d3c66c70646ca4c63876fe2680f82babc5ec63f
Hongs <ihongs@live.cn>
2023-03-26 16:40:04 +0800

    抛弃自定义会话工具, 改为改写 jetty SessionHandler

--- hongs-serv/src/main/java/io/github/ihongs/combat/serv/Records.java
+++ hongs-serv/src/main/java/io/github/ihongs/combat/serv/Records.java
@@ -1,10 +1,8 @@
 package io.github.ihongs.combat.serv;
 
-import io.github.ihongs.HongsException;
 import io.github.ihongs.combat.CombatHelper;
 import io.github.ihongs.combat.anno.Combat;
 import io.github.ihongs.serv.Record;
-import io.github.ihongs.serv.Sesion;
 
 /**
  * 存储维护命令
@@ -36,33 +34,6 @@ public class Records {
           CombatHelper.println("Record ID required!");
         }
           CombatHelper.preview( Record.get(args[0]) );
-    }
-
-    /**
-     * 清除过期的数据
-     * @param args
-     * @throws io.github.ihongs.HongsException
-     */
-    @Combat("clean.sess")
-    public static void cleanSess(String[] args) throws HongsException {
-        long exp = 0;
-        if (args.length != 0) {
-             exp = Integer.parseInt ( args[0] );
-        }
-        Sesion.getRecord().del(System.currentTimeMillis() / 1000 - exp);
-    }
-
-    /**
-     * 预览存储的数据
-     * @param args
-     * @throws io.github.ihongs.HongsException
-     */
-    @Combat("check.sess")
-    public static void checkSess(String[] args) throws HongsException {
-        if (args.length == 0) {
-          CombatHelper.println("Record ID required!");
-        }
-          CombatHelper.preview( Sesion.getRecord().get(args[0]) );
     }
 
 }

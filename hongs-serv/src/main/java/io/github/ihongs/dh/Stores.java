374b954c92159cfa4726995b38e7401f11d86905
Hongs <ihongs@live.cn>
2023-11-04 13:15:51 +0800

    因与 java.lang.Record 重名, 迁移到 io.github.ihongs.dh.Stores

---
+++ hongs-serv/src/main/java/io/github/ihongs/dh/Stores.java
@@ -0,0 +1,121 @@
+package io.github.ihongs.dh;
+
+import io.github.ihongs.Core;
+import io.github.ihongs.CoreConfig;
+import io.github.ihongs.HongsException;
+
+/**
+ * 简单数据存储
+ * @author Hongs
+ */
+public class Stores {
+
+    private static IStored getStored() throws HongsException {
+        String cls = CoreConfig.getInstance().getProperty("core.normal.record.model");
+        if (null == cls || 0 == cls.length()) {
+               cls = JStored.class.getName( );
+        }
+        return (IStored)Core.getInstance(cls);
+    }
+
+    /**
+     * 获取数据
+     * @param key
+     * @return 可用 Synt.declare 辅助得到所需类型
+     */
+    public static Object get(String key) {
+        try {
+            return getStored( ).get(key);
+        }
+        catch (HongsException ex ) {
+            throw ex.toExemption();
+        }
+    }
+
+    /**
+     * 设置数据在某时间失效
+     * @param key
+     * @param val
+     * @param exp 到期时间(秒)
+     */
+    public static void set(String key, Object val, long exp) {
+        try {
+            getStored( ).set(key, val, exp);
+        }
+        catch (HongsException ex ) {
+            throw ex.toExemption();
+        }
+    }
+
+    /**
+     * 设置数据过多久后失效
+     * @param key
+     * @param val
+     * @param exp 剩余时间(秒)
+     */
+    public static void put(String key, Object val, long exp) {
+        exp += System.currentTimeMillis() / 1000;
+        try {
+            getStored( ).set(key, val, exp);
+        }
+        catch (HongsException ex ) {
+            throw ex.toExemption();
+        }
+    }
+
+    /**
+     * 设置数据在某时间失效
+     * @param key
+     * @param exp 到期时间(秒)
+     */
+    public static void set(String key, long exp) {
+        try {
+            getStored( ).set(key, exp);
+        }
+        catch (HongsException ex ) {
+            throw ex.toExemption();
+        }
+    }
+
+    /**
+     * 设置数据过多久后失效
+     * @param key
+     * @param exp 剩余时间(秒)
+     */
+    public static void put(String key, long exp) {
+        exp += System.currentTimeMillis() / 1000;
+        try {
+            getStored( ).set(key, exp);
+        }
+        catch (HongsException ex ) {
+            throw ex.toExemption();
+        }
+    }
+
+    /**
+     * 删除数据
+     * @param key
+     */
+    public static void del(String key) {
+        try {
+            getStored().del( key );
+        }
+        catch (HongsException ex ) {
+            throw ex.toExemption();
+        }
+    }
+
+    /**
+     * 清除数据
+     * @param exp
+     */
+    public static void del( long  exp) {
+        try {
+            getStored().del( exp );
+        }
+        catch (HongsException ex ) {
+            throw ex.toExemption();
+        }
+    }
+
+}
--- hongs-serv/src/main/java/io/github/ihongs/serv/Record.java
+++ hongs-serv/src/main/java/io/github/ihongs/serv/Record.java
@@ -1,35 +1,22 @@
 package io.github.ihongs.serv;
 
-import io.github.ihongs.Core;
-import io.github.ihongs.CoreConfig;
-import io.github.ihongs.HongsException;
+import io.github.ihongs.dh.Stores;
 
 /**
  * 简单数据存储
  * @author Hongs
+ * @deprecated 与 java.lang.Record 重名, 已迁移到 io.github.ihongs.dh.Stores
  */
 public class Record {
-
-    private static IRecord getRecord() throws HongsException {
-        String cls = CoreConfig.getInstance().getProperty("core.normal.record.model");
-        if (null == cls || 0 == cls.length()) {
-               cls = JRecord.class.getName( );
-        }
-        return (IRecord)Core.getInstance(cls);
-    }
 
     /**
      * 获取数据
      * @param key
      * @return 可用 Synt.declare 辅助得到所需类型
+     * @deprecated 改用 Stores.get
      */
     public static Object get(String key) {
-        try {
-            return getRecord( ).get(key);
-        }
-        catch (HongsException ex ) {
-            throw ex.toExemption();
-        }
+        return Stores.get(key);
     }
 
     /**
@@ -37,14 +24,10 @@ public class Record {
      * @param key
      * @param val
      * @param exp 到期时间(秒)
+     * @deprecated 改用 Stores.set
      */
     public static void set(String key, Object val, long exp) {
-        try {
-            getRecord( ).set(key, val, exp);
-        }
-        catch (HongsException ex ) {
-            throw ex.toExemption();
-        }
+        Stores.set(key, val, exp);
     }
 
     /**
@@ -52,70 +35,48 @@ public class Record {
      * @param key
      * @param val
      * @param exp 剩余时间(秒)
+     * @deprecated 改用 Stores.put
      */
     public static void put(String key, Object val, long exp) {
-        exp += System.currentTimeMillis() / 1000;
-        try {
-            getRecord( ).set(key, val, exp);
-        }
-        catch (HongsException ex ) {
-            throw ex.toExemption();
-        }
+        Stores.put(key, val, exp);
     }
 
     /**
      * 设置数据在某时间失效
      * @param key
      * @param exp 到期时间(秒)
+     * @deprecated 改用 Stores.set
      */
     public static void set(String key, long exp) {
-        try {
-            getRecord( ).set(key, exp);
-        }
-        catch (HongsException ex ) {
-            throw ex.toExemption();
-        }
+        Stores.set(key, exp);
     }
 
     /**
      * 设置数据过多久后失效
      * @param key
      * @param exp 剩余时间(秒)
+     * @deprecated 改用 Stores.put
      */
     public static void put(String key, long exp) {
-        exp += System.currentTimeMillis() / 1000;
-        try {
-            getRecord( ).set(key, exp);
-        }
-        catch (HongsException ex ) {
-            throw ex.toExemption();
-        }
+        Stores.put(key, exp);
     }
 
     /**
      * 删除数据
      * @param key
+     * @deprecated 改用 Stores.del
      */
     public static void del(String key) {
-        try {
-            getRecord().del( key );
-        }
-        catch (HongsException ex ) {
-            throw ex.toExemption();
-        }
+        Stores.del(key);
     }
 
     /**
      * 清除数据
      * @param exp
+     * @deprecated 改用 Stores.del
      */
     public static void del( long  exp) {
-        try {
-            getRecord().del( exp );
-        }
-        catch (HongsException ex ) {
-            throw ex.toExemption();
-        }
+        Stores.del(exp);
     }
 
 }

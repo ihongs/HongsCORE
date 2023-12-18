c43b5f9f528e9d3a0bd869d6d1d1bbb5c8ece04e
Hongs <ihongs@live.cn>
2023-12-18 22:28:21 +0800

    恢复退出状态码，规避 windows 下批处理程序执行完不退出

--- hongs-core/src/main/java/io/github/ihongs/combat/CombatRunner.java
+++ hongs-core/src/main/java/io/github/ihongs/combat/CombatRunner.java
@@ -105,14 +105,14 @@ public class CombatRunner implements Runnable
    */
   public static void main(String[] args)
   {
-//  int c = 0;
+    int c = 0;
     try
     {
       exec(init(args) );
     }
     catch (Throwable e)
     {
-/*    if (e instanceof CruxCause)
+      if (e instanceof CruxCause)
       {
         switch (((CruxCause) e).getErrno())
         {
@@ -126,14 +126,13 @@ public class CombatRunner implements Runnable
       {
         c = 5;
       }
-*/
       CoreLogger.error(e);
     }
     finally
     {
       Core.getInstance( ).close( );
       Core.getInterior( ).close( );
-//    System.exit(c);
+      System.exit(c);
     }
   }
 

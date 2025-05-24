46d88a6382e0e6b42584bb8059069cda29f96ff4
Hongs <ihongs@live.cn>
2025-05-24 16:22:35 +0800

    补充注释, 增加辅助识别方法

--- hongs-core/src/main/java/io/github/ihongs/CruxCause.java
+++ hongs-core/src/main/java/io/github/ihongs/CruxCause.java
@@ -7,24 +7,82 @@ package io.github.ihongs;
 public interface CruxCause
 {
 
+    /**
+     * 获取异常代码
+     * @return
+     */
     public int getErrno();
 
+    /**
+     * 获取异常描述
+     * @return
+     */
     public String getError();
 
+    /**
+     * 获取接口状态
+     * @return
+     */
     public int getState();
 
+    /**
+     * 获取接口代码
+     * @return
+     */
     public String getStage();
 
+    /**
+     * 获取异常参数
+     * @return
+     */
     public Object [] getCases();
 
+    /**
+     * 获取异常原因
+     * @return
+     */
     public Throwable getCause();
 
+    /**
+     * 获取异常消息
+     * @return
+     */
     public String getMessage( );
 
+    /**
+     * 获取本地消息
+     * @return
+     */
     public String getLocalizedMessage();
 
+    /**
+     * 转需检查异常
+     * @return
+     */
     public CruxException toException();
 
+    /**
+     * 转无检查异常
+     * @return
+     */
     public CruxExemption toExemption();
+
+    /**
+     * 判断异常类别
+     * @param ern
+     * @return
+     */
+    public default boolean like (int ern) {
+        return getErrno() == ern;
+    }
+
+    /**
+     * 判断异常类别
+     * @param err
+     * @return
+     */
+    public default boolean like (String err) {
+        return getError().equals(err);
+    }
 
 }
